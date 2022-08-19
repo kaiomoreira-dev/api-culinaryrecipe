b63eb20f04d4bd0636b313e60308fabc36137920
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-19 16:08:15 -0300

    add conexao c/ typeorm

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,4 +1,8 @@
+import { createConnection } from "@shared/infra/typeorm";
+
 import { app } from "./app";
+
+createConnection();
 
 app.listen(3333, () => {
   console.log("Server listening on port 3333");
