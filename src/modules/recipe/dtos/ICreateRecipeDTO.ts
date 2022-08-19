f4bd42924fcbb00cccd196917e6412697880653a
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-19 19:22:15 -0300

    create interface DTO c/ atributos da entity

---
+++ src/modules/recipe/dtos/ICreateRecipeDTO.ts
@@ -0,0 +1,13 @@
+export interface ICreateRecipeDTO {
+    id: string;
+    name: string;
+    description: string;
+    ingredients: string[];
+    time: number;
+    difficulty: string;
+    dish_type: string;
+    additional_features: string;
+    total_guests: number;
+    created_at: Date;
+    updated_at: Date;
+}
