4344356cb42d3a392d3b7affa46968294d14c910
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-19 16:14:02 -0300

    create connection typeorm

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,14 +1,20 @@
-import { Connection, createConnection, getConnectionOptions } from "typeorm";
+import { DataSource } from "typeorm";
 
-export default async (): Promise<Connection> => {
-  const defaultOptions = await getConnectionOptions();
+const dataSource = new DataSource({
+  type: "postgres",
+  port: 5432,
+  username: "docker",
+  password: "receitasecreta",
+  database:
+    process.env.NODE_ENV === "test"
+      ? "culinary_recipes_test"
+      : "culinary_recipes",
+  entities: [],
+  migrations: [],
+});
 
-  return createConnection(
-    Object.assign(defaultOptions, {
-      database:
-        process.env.NODE_ENV === "test"
-          ? "culinary_recipes_test"
-          : defaultOptions.database,
-    })
-  );
-};
+export function createConnection(host = "database"): Promise<DataSource> {
+  return dataSource.setOptions({ host }).initialize();
+}
+
+export default dataSource;
