d96b635fc344cfd0af17b012253c0b0905f3dc6b
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-19 10:25:37 -0300

    corrigindo regras de code com eslint

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,8 +1,7 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
 app.listen(3333, () => {
-    console.log("Server listening on port 3333");
-  });
-  
+  console.log("Server listening on port 3333");
+});
