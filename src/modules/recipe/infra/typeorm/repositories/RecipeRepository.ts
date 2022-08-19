e8c5159482b93d39482f8fbe7a4a98ce2a3b2727
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-19 19:14:46 -0300

    create repository Recipe e implemet create

---
+++ src/modules/recipe/infra/typeorm/repositories/RecipeRepository.ts
@@ -0,0 +1,62 @@
+import { ICreateRecipeDTO } from "@modules/recipe/dtos/ICreateRecipeDTO";
+import { IRecipeRepository } from "@modules/recipe/repositories/IRecipeRepository";
+import { Repository } from "typeorm";
+
+import dataSource from "@shared/infra/typeorm";
+
+import { Recipe } from "../entities/Recipe";
+
+export class RecipeRepository implements IRecipeRepository {
+    repository: Repository<Recipe>;
+
+    constructor() {
+        this.repository = dataSource.getRepository(Recipe);
+    }
+
+    async create({
+        id,
+        description,
+        name,
+        additional_features,
+        created_at,
+        difficulty,
+        dish_type,
+        ingredients,
+        time,
+        total_guests,
+        updated_at,
+    }: ICreateRecipeDTO): Promise<Recipe> {
+        const recipe = this.repository.create({
+            id,
+            description,
+            name,
+            additional_features,
+            created_at,
+            difficulty,
+            dish_type,
+            ingredients,
+            time,
+            total_guests,
+            updated_at,
+        });
+
+        this.repository.save(recipe);
+
+        return recipe;
+    }
+    list(): Promise<Recipe[]> {
+        throw new Error("Method not implemented.");
+    }
+    findRecipeById(id: string): Promise<Recipe> {
+        throw new Error("Method not implemented.");
+    }
+    findRecipeByDifficulty(difficulty: string): Promise<Recipe> {
+        throw new Error("Method not implemented.");
+    }
+    updateRecipeById(id: string): Promise<Recipe> {
+        throw new Error("Method not implemented.");
+    }
+    deleteRecipeById(id: string): Promise<Recipe> {
+        throw new Error("Method not implemented.");
+    }
+}
