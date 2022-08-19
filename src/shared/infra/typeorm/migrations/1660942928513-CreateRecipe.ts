44ad0ea652818080de7006068f04527b6021fff7
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-19 18:43:46 -0300

    create Recipe no banco de dados postgres

--- src/shared/infra/typeorm/migrations/1660942928513-CreateRecipe.ts
+++ src/shared/infra/typeorm/migrations/1660942928513-CreateRecipe.ts
@@ -1,11 +1,79 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateRecipe1660942928513 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // id;
+    // name;
+    // description;
+    // ingredients;
+    // time;
+    // difficulty;
+    // dish_type;
+    // additional_features;
+    // total_guests;
+    // created_at;
+    // updated_at;
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+    await queryRunner.createTable(
+      new Table({
+        name: "recipes",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "ingredients",
+            type: "varchar",
+            isArray: true,
+          },
+          {
+            name: "time",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "difficulty",
+            type: "varchar",
+          },
+          {
+            name: "dish_type",
+            type: "varchar",
+          },
+          {
+            name: "additional_features",
+            type: "varchar",
+          },
+          {
+            name: "total_guests",
+            type: "numeric",
+          },
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
 
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("recipes");
+  }
 }
