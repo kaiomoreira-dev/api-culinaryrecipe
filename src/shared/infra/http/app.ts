f950be5cf68687ce94b4d102aeacd9e09dae657b
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-20 09:28:22 -0300

    importando rotas, instancias e json

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,4 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import express from "express";
 
+import "@shared/container";
+
+import { routes } from "@shared/infra/http/routes";
+
 export const app = express();
+
+app.use(express.json());
+
+app.use(routes);
