0e6fdea320243676e1a954d1cb43108fe531bc53
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-20 15:16:42 -0300

    removendo created e updated

--- src/modules/recipe/dtos/ICreateRecipeDTO.ts
+++ src/modules/recipe/dtos/ICreateRecipeDTO.ts
@@ -2,12 +2,10 @@ export interface ICreateRecipeDTO {
     id?: string;
     name: string;
     description: string;
-    ingredients: string[];
+    ingredients: string;
     time: number;
     difficulty: string;
     dish_type: string;
     additional_features: string;
     total_guests: number;
-    created_at?: Date;
-    updated_at?: Date;
 }
