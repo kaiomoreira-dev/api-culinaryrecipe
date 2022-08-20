67be9f3f8f2ca52004947025e581ac9d6db4b8d9
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-20 08:56:59 -0300

    controller retornando recipe criada em useCase

---
+++ src/modules/recipe/useCases/createRecipe/CreateRecipeController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { CreateRecipeUseCase } from "./CreateRecipeUseCase";
+
+export class CreateRecipeController {
+    async handle(request: Request, response: Response): Promise<Response> {
+        const {
+            name,
+            description,
+            ingredients,
+            difficulty,
+            dish_type,
+            additional_features,
+            time,
+            total_guests,
+        } = request.body;
+
+        const createRecipeUseCase = container.resolve(CreateRecipeUseCase);
+
+        const recipe = createRecipeUseCase.execute({
+            name,
+            description,
+            ingredients,
+            difficulty,
+            dish_type,
+            additional_features,
+            time,
+            total_guests,
+        });
+
+        return response.status(200).json(recipe);
+    }
+}
