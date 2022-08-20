a81bf503160ad83ced0e542ac2a78303e44a3554
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-20 08:52:02 -0300

    create metodo execute c/ metodo create

--- src/modules/recipe/useCases/createRecipe/CreateRecipeUseCase.ts
+++ src/modules/recipe/useCases/createRecipe/CreateRecipeUseCase.ts
@@ -1,3 +1,5 @@
+import { ICreateRecipeDTO } from "@modules/recipe/dtos/ICreateRecipeDTO";
+import { Recipe } from "@modules/recipe/infra/typeorm/entities/Recipe";
 import { IRecipeRepository } from "@modules/recipe/repositories/IRecipeRepository";
 import { inject, injectable } from "tsyringe";
 
@@ -7,4 +9,30 @@ export class CreateRecipeUseCase {
         @inject("RecipeRepository")
         private recipeRepository: IRecipeRepository
     ) {}
+
+    async execute({
+        id,
+        name,
+        description,
+        ingredients,
+        difficulty,
+        dish_type,
+        additional_features,
+        time,
+        total_guests,
+    }: ICreateRecipeDTO): Promise<Recipe> {
+        const recipe = this.recipeRepository.create({
+            id,
+            name,
+            description,
+            ingredients,
+            difficulty,
+            dish_type,
+            additional_features,
+            time,
+            total_guests,
+        });
+
+        return recipe;
+    }
 }
