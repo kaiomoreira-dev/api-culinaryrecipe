fc875198a333b0c3aaa44f7c6f079a4210b936e4
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-20 22:43:25 -0300

    create interface com os metodos do repo

---
+++ src/modules/recipe/repositories/IIngredientRepository.ts
@@ -0,0 +1,12 @@
+import { ICreateIngredientsDTO } from "../dtos/ICreateIngredientsDTO";
+import { Ingredient } from "../infra/typeorm/entities/Ingredient";
+
+export interface IIngredientRepository {
+    create(data: ICreateIngredientsDTO): Promise<Ingredient>;
+    list(): Promise<Ingredient[]>;
+    findAllIngredientsByAnimal(animal: string): Promise<Ingredient[]>;
+    findIgredientById(id: string): Promise<Ingredient>;
+    findAllIngredientsByName(name: string): Promise<Ingredient[]>;
+    updateAnimalById(id: string): Promise<Ingredient>;
+    deleteIngredientById(id: string): Promise<Ingredient>;
+}
