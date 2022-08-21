56572f4f8fb16919edf4a2a1903f3db55ba6d184
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-20 22:42:40 -0300

    create interface c/ atributos de ingredients

---
+++ src/modules/recipe/dtos/ICreateIngredientsDTO.ts
@@ -0,0 +1,9 @@
+export interface ICreateIngredientsDTO {
+    id?: string;
+    description: string;
+    name: string;
+    weight: number;
+    unity: number;
+    animal?: string;
+    color?: string;
+}
