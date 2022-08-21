57b6c684ce908457d3d345532d2ac1f8be2154d2
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-21 10:19:35 -0300

    implement metodo handle p/ retornar ingredients

--- src/modules/recipe/useCases/listIngredient/ListIngredientController.ts
+++ src/modules/recipe/useCases/listIngredient/ListIngredientController.ts
@@ -1,5 +1,14 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { ListIngredientUseCase } from "./ListIngredientUseCase";
 
 export class ListIngredientController {
-    async handle(request: Request, response: Response): Promise<Response> {}
+    async handle(request: Request, response: Response): Promise<Response> {
+        const listIngredientUseCase = container.resolve(ListIngredientUseCase);
+
+        const ingredients = await listIngredientUseCase.execute();
+
+        return response.status(200).json(ingredients);
+    }
 }
