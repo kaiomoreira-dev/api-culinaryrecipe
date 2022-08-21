85cd60c8e0d836f37a73089797c13d5852d71efb
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-21 10:21:42 -0300

    create rota get p/ retornar ingredients

--- src/shared/infra/http/routes/ingredient.routes.ts
+++ src/shared/infra/http/routes/ingredient.routes.ts
@@ -1,8 +1,13 @@
 import { CreateIngredientController } from "@modules/recipe/useCases/createIngredient/CreateIngredientController";
+import { ListIngredientController } from "@modules/recipe/useCases/listIngredient/ListIngredientController";
 import { Router } from "express";
 
 export const ingredientRoutes = Router();
 
 const createIngredientController = new CreateIngredientController();
 
+const listIngredientController = new ListIngredientController();
+
 ingredientRoutes.post("/", createIngredientController.handle);
+
+ingredientRoutes.get("/", listIngredientController.handle);
