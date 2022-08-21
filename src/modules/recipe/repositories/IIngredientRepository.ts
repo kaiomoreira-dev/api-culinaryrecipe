b7fc7964797d0432891b2efc7ea7a36617c27457
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-21 10:50:55 -0300

    alter name do metodo e remove retorno de array

--- src/modules/recipe/repositories/IIngredientRepository.ts
+++ src/modules/recipe/repositories/IIngredientRepository.ts
@@ -4,9 +4,9 @@ import { Ingredient } from "../infra/typeorm/entities/Ingredient";
 export interface IIngredientRepository {
     create(data: ICreateIngredientDTO): Promise<Ingredient>;
     list(): Promise<Ingredient[]>;
-    findAllIngredientsByAnimal(animal: string): Promise<Ingredient[]>;
+    findIngredientByAnimal(animal: string): Promise<Ingredient>;
     findIngredientById(id: string): Promise<Ingredient>;
-    findAllIngredientsByName(name: string): Promise<Ingredient[]>;
+    findIngredientByName(name: string): Promise<Ingredient>;
     updateAnimalById(id: string, animal: string): Promise<Ingredient>;
     deleteIngredientById(id: string): Promise<void>;
 }
