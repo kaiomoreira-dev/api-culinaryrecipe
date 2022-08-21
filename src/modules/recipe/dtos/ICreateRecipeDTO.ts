d910156e3f447c6cc595ab7905190a12c5a02666
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-21 10:38:21 -0300

    alter ingredients p/ lista de ingredients

--- src/modules/recipe/dtos/ICreateRecipeDTO.ts
+++ src/modules/recipe/dtos/ICreateRecipeDTO.ts
@@ -1,8 +1,10 @@
+import { Ingredient } from "../infra/typeorm/entities/Ingredient";
+
 export interface ICreateRecipeDTO {
     id?: string;
     name: string;
     description: string;
-    ingredients: string;
+    ingredients: Ingredient[];
     time: number;
     difficulty: string;
     dish_type: string;
