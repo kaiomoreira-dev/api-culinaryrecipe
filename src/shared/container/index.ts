c7e697c2743d28780f568c89808677e7a54ccc5a
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-20 22:59:32 -0300

    create instancia sigleton de ingredient

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,15 @@
+import { IngredientRepository } from "@modules/recipe/infra/typeorm/repositories/IngredientRepository";
 import { RecipeRepository } from "@modules/recipe/infra/typeorm/repositories/RecipeRepository";
+import { IIngredientRepository } from "@modules/recipe/repositories/IIngredientRepository";
 import { IRecipeRepository } from "@modules/recipe/repositories/IRecipeRepository";
 import { container } from "tsyringe";
 
 container.registerSingleton<IRecipeRepository>(
-    "RecipeRepository",
-    RecipeRepository
+  "RecipeRepository",
+  RecipeRepository
+);
+
+container.registerSingleton<IIngredientRepository>(
+  "IngredientRepository",
+  IngredientRepository
 );
