239e993cbc6910486318455ecc1011fbfddf991c
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-20 21:24:25 -0300

    create migration ingredient

--- src/shared/infra/typeorm/migrations/1661028123973-CreateIngredient.ts
+++ src/shared/infra/typeorm/migrations/1661028123973-CreateIngredient.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateIngredient1661028123973 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
@@ -8,10 +8,59 @@ export class CreateIngredient1661028123973 implements MigrationInterface {
     // weight: number;
     // unity:number;
     // animal?:string;
-    // cor?:string;
+    // color?:string;
     // created_at: string;
     // updated_at: string;
+
+    await queryRunner.createTable(
+      new Table({
+        name: "ingredients",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "weight",
+            type: "numeric",
+          },
+          {
+            name: "unity",
+            type: "numeric",
+          },
+          {
+            name: "animal",
+            type: "varchar",
+          },
+          {
+            name: "color",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("ingredients");
+  }
 }
