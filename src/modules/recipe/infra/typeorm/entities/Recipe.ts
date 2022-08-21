4c8d475a219c4e48b4216d2f2fdac7a979544df8
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-20 21:36:51 -0300

    create relacionamento manytomany c/ ingredients

--- src/modules/recipe/infra/typeorm/entities/Recipe.ts
+++ src/modules/recipe/infra/typeorm/entities/Recipe.ts
@@ -2,10 +2,14 @@ import {
     Column,
     CreateDateColumn,
     Entity,
+    JoinTable,
+    ManyToMany,
     PrimaryColumn,
     UpdateDateColumn,
 } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
+
+import { Ingredient } from "./Ingredient";
 
 @Entity("recipes")
 export class Recipe {
@@ -18,8 +22,14 @@ export class Recipe {
     @Column()
     description: string;
 
+    @ManyToMany(() => Ingredient, (ingredient) => ingredient.id)
+    @JoinTable({
+        name: "recipes_ingredients",
+        joinColumns: [{ name: "recipe_id" }],
+        inverseJoinColumns: [{ name: "ingredient_id" }],
+    })
     @Column()
-    ingredients: string;
+    ingredients: Ingredient;
 
     @Column()
     time: number;
