ba12739ec0e131e026bd413ca43e08b106c4d6d0
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-20 23:17:31 -0300

    criando gerenciamento p/ rota de ingredients

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 
+import { ingredientRoutes } from "./ingredient.routes";
 import { recipeRoutes } from "./recipe.routes";
 
 export const routes = Router();
 
 routes.use("/recipe", recipeRoutes);
+routes.use("/ingredient", ingredientRoutes);
