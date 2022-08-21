363e7f86203cfdb434999969b2e00707a759f6b8
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-20 23:07:41 -0300

    create metodo execute p/ criar ingredient

--- src/modules/recipe/useCases/createIngredient/CreateIngredientUseCase.ts
+++ src/modules/recipe/useCases/createIngredient/CreateIngredientUseCase.ts
@@ -1,3 +1,5 @@
+import { ICreateIngredientDTO } from "@modules/recipe/dtos/ICreateIngredientDTO";
+import { Ingredient } from "@modules/recipe/infra/typeorm/entities/Ingredient";
 import { IIngredientRepository } from "@modules/recipe/repositories/IIngredientRepository";
 import { inject, injectable } from "tsyringe";
 
@@ -7,4 +9,24 @@ export class CreateIngredient {
         @inject("IngredientRepository")
         private ingredientRepository: IIngredientRepository
     ) {}
+
+    async execute({
+        name,
+        description,
+        unity,
+        weight,
+        animal,
+        color,
+    }: ICreateIngredientDTO): Promise<Ingredient> {
+        const ingredient = await this.ingredientRepository.create({
+            name,
+            description,
+            unity,
+            weight,
+            animal,
+            color,
+        });
+
+        return ingredient;
+    }
 }
