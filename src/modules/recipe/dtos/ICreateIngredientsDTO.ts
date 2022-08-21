aaa923e9c4cfcac9d82e20761b6a339b6a9ccafa
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-20 23:09:30 -0300

    remove 's' da nomeclatura

--- src/modules/recipe/dtos/ICreateIngredientsDTO.ts
+++
@@ -1,9 +0,0 @@
-export interface ICreateIngredientsDTO {
-    id?: string;
-    description: string;
-    name: string;
-    weight: number;
-    unity: number;
-    animal?: string;
-    color?: string;
-}
