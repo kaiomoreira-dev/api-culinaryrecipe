f556b75dbaaae1a8463192c751cf012ee1bdec97
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-21 10:12:34 -0300

    create use and instancia d Ingredient

---
+++ src/modules/recipe/useCases/listIngredient/ListIngredientUseCase.ts
@@ -0,0 +1,11 @@
+import { Ingredient } from "@modules/recipe/infra/typeorm/entities/Ingredient";
+import { IIngredientRepository } from "@modules/recipe/repositories/IIngredientRepository";
+import { inject, injectable } from "tsyringe";
+
+@injectable()
+export class ListIngredientUseCase {
+    constructor(
+        @inject("IngredientRepository")
+        private ingredientRepository: IIngredientRepository
+    ) {}
+}
