af7877b7b2e1d22b51cf8d8edf0d1c02d4f79240
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-23 20:43:32 -0300

    corrindo duplicata

--- src/modules/author/infra/typeorm/repositories/IEmailRepository.ts
+++
@@ -1,8 +0,0 @@
-import { ICreateEmailDTO } from "@modules/author/dtos/ICreateEmailDTO";
-
-import { Email } from "../entities/Email";
-
-export interface IEmailRepository {
-    create(data: ICreateEmailDTO): Promise<Email>;
-    list(): Promise<Email[]>;
-}
