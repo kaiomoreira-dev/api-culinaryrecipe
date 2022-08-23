124372fe7b85f56f2c79a0d86b3b73bdcd11fe26
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-23 20:45:40 -0300

    importando serviço de repositorio do typeorm

--- src/modules/author/infra/typeorm/repositories/EmailRepository.ts
+++ src/modules/author/infra/typeorm/repositories/EmailRepository.ts
@@ -1,9 +1,17 @@
 import { ICreateEmailDTO } from "@modules/author/dtos/ICreateEmailDTO";
 import { IEmailRepository } from "@modules/author/repositories/IEmailRepository";
+import { Repository } from "typeorm";
+
+import dataSource from "@shared/infra/typeorm";
 
 import { Email } from "../entities/Email";
 
 export class EmailRepository implements IEmailRepository {
+    private repository: Repository<Email>;
+
+    constructor() {
+        this.repository = dataSource.getRepository(Email);
+    }
     create(data: ICreateEmailDTO): Promise<Email> {
         throw new Error("Method not implemented.");
     }
