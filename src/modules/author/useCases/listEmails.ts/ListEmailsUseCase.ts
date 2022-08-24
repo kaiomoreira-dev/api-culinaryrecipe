8d6fdee911586b1aa2504578f71386ae895261c2
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-24 20:05:02 -0300

    create list case p;/ retornar todos os emails

--- src/modules/author/useCases/listEmails.ts/ListEmailsUseCase.ts
+++ src/modules/author/useCases/listEmails.ts/ListEmailsUseCase.ts
@@ -1,6 +1,17 @@
-import { injectable } from "tsyringe";
+import { Email } from "@modules/author/infra/typeorm/entities/Email";
+import { IEmailRepository } from "@modules/author/repositories/IEmailRepository";
+import { inject, injectable } from "tsyringe";
 
 @injectable()
 export class ListEmailsUseCase {
-    constructor() {}
+    constructor(
+        @inject("EmailRepository")
+        private emailRepository: IEmailRepository
+    ) {}
+
+    async execute(): Promise<Email[]> {
+        const emails = await this.emailRepository.list();
+
+        return emails;
+    }
 }
