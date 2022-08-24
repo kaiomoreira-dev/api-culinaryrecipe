d512d777f6f2f6744778f96c31d93b2667400724
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-23 22:34:19 -0300

    create test p/ validar se email ja existe ao criar

--- src/modules/author/useCases/createEmail.ts/CreateEmailUseCase.spec.ts
+++ src/modules/author/useCases/createEmail.ts/CreateEmailUseCase.spec.ts
@@ -2,6 +2,8 @@ import { faker } from "@faker-js/faker";
 import { ICreateEmailDTO } from "@modules/author/dtos/ICreateEmailDTO";
 import { EmailRepositoryInMemory } from "@modules/author/repositories/in-Memory/EmailRepositoryInMemory";
 
+import { AppError } from "@shared/errors/AppError";
+
 import { CreateEmailUseCase } from "./CreateEmailUseCase";
 
 let emailRepositoryInMemory: EmailRepositoryInMemory;
@@ -22,5 +24,23 @@ describe("Create e-mail UseCase", () => {
         const e_mail = await createEmailUseCase.execute(email);
 
         expect(e_mail).toHaveProperty("id");
+    });
+
+    it("should not be able to create a new e-mail with email already exists", async () => {
+        const email1: ICreateEmailDTO = {
+            id: faker.datatype.uuid(),
+            e_mail: "test@test.com.br",
+        };
+
+        const email2: ICreateEmailDTO = {
+            id: faker.datatype.uuid(),
+            e_mail: "test@test.com.br",
+        };
+
+        const e_mail1 = await createEmailUseCase.execute(email1);
+
+        await expect(createEmailUseCase.execute(email2)).rejects.toEqual(
+            new AppError("Email is already exists.", 401)
+        );
     });
 });
