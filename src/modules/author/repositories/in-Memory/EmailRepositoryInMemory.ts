646d1afcbb372e74fb2534f6b8ae89767c77c021
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-23 22:06:04 -0300

    create const array repo do tipo Email

--- src/modules/author/repositories/in-Memory/EmailRepositoryInMemory.ts
+++ src/modules/author/repositories/in-Memory/EmailRepositoryInMemory.ts
@@ -4,6 +4,8 @@ import { Email } from "@modules/author/infra/typeorm/entities/Email";
 import { IEmailRepository } from "../IEmailRepository";
 
 export class EmailRepositoryInMemory implements IEmailRepository {
+    private repository: Email[] = [];
+
     create(data: ICreateEmailDTO): Promise<Email> {
         throw new Error("Method not implemented.");
     }
