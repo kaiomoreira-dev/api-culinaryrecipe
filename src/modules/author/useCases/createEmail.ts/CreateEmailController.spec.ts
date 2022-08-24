580d322712f55c7443a615edd8fb16cac925b248
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-23 23:18:17 -0300

    create controller teste p/ criar email

---
+++ src/modules/author/useCases/createEmail.ts/CreateEmailController.spec.ts
@@ -0,0 +1,34 @@
+import "dotenv/config";
+import { faker } from "@faker-js/faker";
+import request from "supertest";
+import { DataSource } from "typeorm";
+
+import { app } from "@shared/infra/http/app";
+import { createConnection } from "@shared/infra/typeorm";
+
+let connection: DataSource;
+
+describe("Create Email Controller", () => {
+    beforeAll(async () => {
+        connection = await createConnection("localhost");
+
+        await connection.runMigrations();
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+
+        await connection.destroy();
+    });
+
+    it("should be able to create a new email", async () => {
+        const response = await request(app).post("/email").send({
+            id: faker.datatype.uuid(),
+            e_mail: faker.internet.email(),
+        });
+
+        console.log(response.body);
+
+        expect(response.status).toEqual(200);
+    });
+});
