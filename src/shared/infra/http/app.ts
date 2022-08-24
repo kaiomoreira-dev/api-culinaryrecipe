55439a62a35e0fdcaec69986f1a2e4cc8b74bf3b
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-23 21:40:57 -0300

    configura app p/ retornar error code limpo

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,8 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 import "@shared/container";
 
+import "express-async-errors";
+
+import { AppError } from "@shared/errors/AppError";
 import { routes } from "@shared/infra/http/routes";
 
 export const app = express();
@@ -10,3 +13,16 @@ export const app = express();
 app.use(express.json());
 
 app.use(routes);
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server - error: ${err.message}`,
+    });
+  }
+);
