966d576d5d43f8883c15fcc5a125b7c26fcced0b
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-24 20:09:23 -0300

    create rota get p/ retornar todos os emails

--- src/shared/infra/http/routes/email.routes.ts
+++ src/shared/infra/http/routes/email.routes.ts
@@ -1,8 +1,13 @@
 import { CreateEmailController } from "@modules/author/useCases/createEmail.ts/CreateEmailController";
+import { ListEmailsController } from "@modules/author/useCases/listEmails.ts/ListEmailsController";
 import { Router } from "express";
 
 export const emailRoutes = Router();
 
-export const createEmailController = new CreateEmailController();
+const createEmailController = new CreateEmailController();
+
+const listEmailsController = new ListEmailsController();
 
 emailRoutes.post("/", createEmailController.handle);
+
+emailRoutes.get("/", listEmailsController.handle);
