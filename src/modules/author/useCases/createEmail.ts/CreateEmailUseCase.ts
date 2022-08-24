421c209c110b3dcb5ae30159d5df55015e53835e
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-23 21:04:50 -0300

    alter case p/ criar email validar se existe igual

--- src/modules/author/useCases/createEmail.ts/CreateEmailUseCase.ts
+++ src/modules/author/useCases/createEmail.ts/CreateEmailUseCase.ts
@@ -1,5 +1,9 @@
+import { ICreateEmailDTO } from "@modules/author/dtos/ICreateEmailDTO";
+import { Email } from "@modules/author/infra/typeorm/entities/Email";
 import { IEmailRepository } from "@modules/author/repositories/IEmailRepository";
 import { inject, injectable } from "tsyringe";
+
+import { AppError } from "@shared/errors/AppError";
 
 @injectable()
 export class CreateEmailUseCase {
@@ -7,4 +11,18 @@ export class CreateEmailUseCase {
         @inject("EmailRepository")
         private emailRepository: IEmailRepository
     ) {}
+
+    async execute({ id, email }: ICreateEmailDTO): Promise<Email> {
+        const emailValidator = await this.emailRepository.findEmailByE_mail(
+            email
+        );
+
+        if (emailValidator) {
+            throw new AppError("Email is already exists.", 401);
+        }
+
+        const e_mail = await this.emailRepository.create({ id, email });
+
+        return e_mail;
+    }
 }
