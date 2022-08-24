5922e2374f14d0b4920d363b9aba6e2cb2d21acb
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-24 16:09:38 -0300

    config jest p/ capturar apenas useCases

--- jest.config.ts
+++ jest.config.ts
@@ -24,7 +24,7 @@ export default {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: ["<rootDir>/src/modules/**/useCases/**/*.ts"],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
