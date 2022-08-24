dbbc453d215763aec3da6a19eee7e963b918043a
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-24 20:46:24 -0300

    create interface c/ atributor de author

---
+++ src/modules/author/dtos/ICreateAuthorDTO.ts
@@ -0,0 +1,15 @@
+import { Recipe } from "@modules/recipe/infra/typeorm/entities/Recipe";
+
+import { Email } from "../infra/typeorm/entities/Email";
+
+export interface ICreateAuthorDTO {
+    id?: string;
+
+    name: string;
+
+    whatsapp: string;
+
+    recipes?: Recipe[];
+
+    emails?: Email[];
+}
