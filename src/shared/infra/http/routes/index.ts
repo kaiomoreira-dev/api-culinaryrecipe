bb2b57da5ea558d7f9ec0d4d69baee2e448b508b
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-23 21:13:34 -0300

    add rotas email ao gerenciamento de rotas

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { emailRoutes } from "./email.routes";
 import { ingredientRoutes } from "./ingredient.routes";
 import { recipeRoutes } from "./recipe.routes";
 
@@ -7,3 +8,4 @@ export const routes = Router();
 
 routes.use("/recipe", recipeRoutes);
 routes.use("/ingredient", ingredientRoutes);
+routes.use("/email", emailRoutes);
