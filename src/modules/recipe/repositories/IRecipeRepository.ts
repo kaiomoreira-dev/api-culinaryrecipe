4d7baecd70df581d5573b0f8065286e4a5e65a9d
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-25 18:08:16 -0300

    alter authorName p/ author_name

--- src/modules/recipe/repositories/IRecipeRepository.ts
+++ src/modules/recipe/repositories/IRecipeRepository.ts
@@ -7,7 +7,7 @@ export interface IRecipeRepository {
 
     findRecipeById(id: string): Promise<Recipe>;
     findRecipesByDifficulty(difficulty: string): Promise<Recipe[]>;
-    findRecipeByAuthor(author_id: string): Promise<Recipe>;
+    findRecipeByAuthor(author_name: string): Promise<Recipe>;
     findRecipeByName(name: string): Promise<Recipe>;
 
     updateRecipeTimeById(id: string, time: number): Promise<Recipe>;
