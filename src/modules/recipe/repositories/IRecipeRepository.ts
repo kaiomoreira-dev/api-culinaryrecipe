569df24a1099afee9d7287de7fd1ab555f444984
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-25 12:06:55 -0300

    remove metodo update

--- src/modules/recipe/repositories/IRecipeRepository.ts
+++ src/modules/recipe/repositories/IRecipeRepository.ts
@@ -4,10 +4,13 @@ import { Recipe } from "../infra/typeorm/entities/Recipe";
 export interface IRecipeRepository {
     create(data: ICreateRecipeDTO): Promise<Recipe>;
     list(): Promise<Recipe[]>;
+
     findRecipeById(id: string): Promise<Recipe>;
     findRecipesByDifficulty(difficulty: string): Promise<Recipe[]>;
     findRecipeByAuthor(author_id: string): Promise<Recipe>;
     findRecipeByName(name: string): Promise<Recipe>;
+
     updateRecipeTimeById(id: string, time: number): Promise<Recipe>;
+
     deleteRecipeById(id: string): Promise<void>;
 }
