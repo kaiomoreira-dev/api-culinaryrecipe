0fe791b12c2b81e8baf8f503129ae540d4c721eb
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-24 22:19:14 -0300

    change email p/ e_mail

--- src/modules/author/repositories/IAuthorRepository.ts
+++ src/modules/author/repositories/IAuthorRepository.ts
@@ -4,7 +4,7 @@ import { Author } from "../infra/typeorm/entities/Author";
 export interface IAuthorRepository {
     create(data: ICreateAuthorDTO): Promise<Author>;
     list(): Promise<Author[]>;
-    findAuthorByEmail(email: string): Promise<Author>;
+    findAuthorByEmail(e_mail: string): Promise<Author>;
     findAuthorByName(name: string): Promise<Author>;
     deleteAuthorById(id: string): Promise<Author>;
 }
