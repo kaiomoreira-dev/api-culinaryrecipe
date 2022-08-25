02e09e08c8a28fe755031966ac10ffd4b4abe371
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-24 22:25:38 -0300

    criando intancia singleton de author em container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,4 +1,6 @@
+import { AuthorRepository } from "@modules/author/infra/typeorm/repositories/AuthorRepository";
 import { EmailRepository } from "@modules/author/infra/typeorm/repositories/EmailRepository";
+import { IAuthorRepository } from "@modules/author/repositories/IAuthorRepository";
 import { IEmailRepository } from "@modules/author/repositories/IEmailRepository";
 import { IngredientRepository } from "@modules/recipe/infra/typeorm/repositories/IngredientRepository";
 import { RecipeRepository } from "@modules/recipe/infra/typeorm/repositories/RecipeRepository";
@@ -19,4 +21,9 @@ container.registerSingleton<IIngredientRepository>(
 container.registerSingleton<IEmailRepository>(
   "EmailRepository",
   EmailRepository
+);
+
+container.registerSingleton<IAuthorRepository>(
+  "AuthorRepository",
+  AuthorRepository
 );
