cbc31a3b4d5a5de1c86575ac8739370c75f7adfe
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-24 22:59:35 -0300

    create controller p/ criar autor e retornar

---
+++ src/modules/author/useCases/createAuthor/CreateAuthorController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { CreateAuthorUseCase } from "./CreateAuthorUseCase";
+
+export class CreateAuthorController {
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { name, whatsapp, emails, recipes } = request.body;
+
+        const createAuthorUseCase = container.resolve(CreateAuthorUseCase);
+
+        const author = await createAuthorUseCase.execute({
+            name,
+            whatsapp,
+            emails,
+            recipes,
+        });
+
+        return response.status(200).json(author);
+    }
+}
