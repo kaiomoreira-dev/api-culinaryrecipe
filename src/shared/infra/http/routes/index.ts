89b3f8e98b032aa972cdf2fb75948027dfe8f3a5
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-25 09:47:48 -0300

    create rota de gerenciamento p/ author

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { authorRoutes } from "./author.routes";
 import { emailRoutes } from "./email.routes";
 import { ingredientRoutes } from "./ingredient.routes";
 import { recipeRoutes } from "./recipe.routes";
@@ -9,3 +10,4 @@ export const routes = Router();
 routes.use("/recipe", recipeRoutes);
 routes.use("/ingredient", ingredientRoutes);
 routes.use("/email", emailRoutes);
+routes.use("/author", authorRoutes);
