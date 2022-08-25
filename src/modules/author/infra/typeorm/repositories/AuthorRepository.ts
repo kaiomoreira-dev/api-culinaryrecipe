63464717810b86d69e1e5d91ee297eaa3153f742
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-24 22:02:35 -0300

    create serviço de repo para author com typeorm

--- src/modules/author/infra/typeorm/repositories/AuthorRepository.ts
+++ src/modules/author/infra/typeorm/repositories/AuthorRepository.ts
@@ -1,9 +1,18 @@
 import { ICreateAuthorDTO } from "@modules/author/dtos/ICreateAuthorDTO";
 import { IAuthorRepository } from "@modules/author/repositories/IAuthorRepository";
+import { Repository } from "typeorm";
+
+import dataSource from "@shared/infra/typeorm";
 
 import { Author } from "../entities/Author";
 
 export class AuthorRepository implements IAuthorRepository {
+    private repository: Repository<Author>;
+
+    constructor() {
+        this.repository = dataSource.getRepository(Author);
+    }
+
     create(data: ICreateAuthorDTO): Promise<Author> {
         throw new Error("Method not implemented.");
     }
