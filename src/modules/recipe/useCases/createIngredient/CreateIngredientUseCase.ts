a8ec24add4864a444df1e1fa46c9a8580a61e358
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-26 15:58:46 -0300

    passando id como opcional p/ retorno

--- src/modules/recipe/useCases/createIngredient/CreateIngredientUseCase.ts
+++ src/modules/recipe/useCases/createIngredient/CreateIngredientUseCase.ts
@@ -11,12 +11,14 @@ export class CreateIngredientUseCase {
     ) {}
 
     async execute({
+        id,
         produto_name,
         description,
         unity,
         weight,
     }: ICreateIngredientDTO): Promise<Ingredient> {
         const ingredient = await this.ingredientRepository.create({
+            id,
             produto_name,
             description,
             unity,
