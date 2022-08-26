63ed7b11d7b0a417f1312f7b8683ef1b69723847
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-26 15:58:13 -0300

    validando se produto ja existe

--- src/modules/recipe/useCases/createProduto/CreateProdutoUseCase.ts
+++ src/modules/recipe/useCases/createProduto/CreateProdutoUseCase.ts
@@ -1,5 +1,9 @@
+import { ICreateProdutoDTO } from "@modules/recipe/dtos/ICreateProdutoDTO";
+import { Produto } from "@modules/recipe/infra/typeorm/entities/Produto";
 import { IProdutoRepository } from "@modules/recipe/repositories/IProdutoRepository";
 import { inject, injectable } from "tsyringe";
+
+import { AppError } from "@shared/errors/AppError";
 
 @injectable()
 export class CreateProdutoUseCase {
@@ -7,4 +11,18 @@ export class CreateProdutoUseCase {
         @inject("ProdutoRepository")
         private produtoRepository: IProdutoRepository
     ) {}
+
+    async execute({
+        id,
+        name,
+        description,
+    }: ICreateProdutoDTO): Promise<Produto> {
+        const produtoValidator = await this.produtoRepository.findProdutoByName(
+            name
+        );
+
+        if (produtoValidator) {
+            throw new AppError("Produto already exists", 401);
+        }
+    }
 }
