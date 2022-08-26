0a66b4af7d563b1205c90a2991e9c906e9fd7411
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-26 16:31:22 -0300

    create rota get p/ retornar produtos

--- src/shared/infra/http/routes/produto.routes.ts
+++ src/shared/infra/http/routes/produto.routes.ts
@@ -1,8 +1,13 @@
 import { CreateProdutoController } from "@modules/recipe/useCases/createProduto/CreateProdutoController";
+import { ListProdutosController } from "@modules/recipe/useCases/listProdutos/ListProdutosController";
 import { Router } from "express";
 
 export const produtoRoutes = Router();
 
 const createProdutoController = new CreateProdutoController();
 
+const listProdutosController = new ListProdutosController();
+
 produtoRoutes.post("/", createProdutoController.handle);
+
+produtoRoutes.get("/produto", listProdutosController.handle);
