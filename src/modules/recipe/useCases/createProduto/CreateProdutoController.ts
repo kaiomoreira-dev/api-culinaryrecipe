301b6ed7c71de30a3aeabb50b2dc01e4098b1d8f
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-26 16:07:29 -0300

    implement metodo handle p/ retornar produto criado

--- src/modules/recipe/useCases/createProduto/CreateProdutoController.ts
+++ src/modules/recipe/useCases/createProduto/CreateProdutoController.ts
@@ -1,5 +1,19 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { CreateProdutoUseCase } from "./CreateProdutoUseCase";
 
 export class CreateProdutoController {
-    async handle(request: Request, response: Response): Promise<Response> {}
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { name, description } = request.body;
+
+        const createProdutoUseCase = container.resolve(CreateProdutoUseCase);
+
+        const produto = await createProdutoUseCase.execute({
+            name,
+            description,
+        });
+
+        return response.status(200).json(produto);
+    }
 }
