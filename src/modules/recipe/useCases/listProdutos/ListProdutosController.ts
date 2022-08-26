960c105d2bebcd7dbe1f128bc78ed8a7e9b9e621
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-26 16:29:47 -0300

    implement handle p/ retornar todos produtos

--- src/modules/recipe/useCases/listProdutos/ListProdutosController.ts
+++ src/modules/recipe/useCases/listProdutos/ListProdutosController.ts
@@ -1,5 +1,19 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { CreateProdutoUseCase } from "../createProduto/CreateProdutoUseCase";
 
 export class ListProdutosController {
-    async handle(request: Request, response: Response): Promise<Response> {}
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { name, description } = request.body;
+
+        const createProdutoUseCase = container.resolve(CreateProdutoUseCase);
+
+        const produtos = await createProdutoUseCase.execute({
+            name,
+            description,
+        });
+
+        return response.status(200).json(produtos);
+    }
 }
