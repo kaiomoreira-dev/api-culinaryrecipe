5d8df2a65a82cd0f0689bbc12ff2137d36972a95
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-26 14:42:11 -0300

    create repository de Produto com typeorm

--- src/modules/author/infra/typeorm/repositories/ProdutoRepository.ts
+++ src/modules/author/infra/typeorm/repositories/ProdutoRepository.ts
@@ -1,7 +1,17 @@
 import { Ingredient } from "@modules/recipe/infra/typeorm/entities/Ingredient";
+import { Produto } from "@modules/recipe/infra/typeorm/entities/Produto";
 import { IProdutoRepository } from "@modules/recipe/infra/typeorm/repositories/IProdutoRepository";
+import { Repository } from "typeorm";
+
+import dataSource from "@shared/infra/typeorm";
 
 export class ProdutoRepository implements IProdutoRepository {
+    private repository: Repository<Produto>;
+
+    constructor() {
+        this.repository = dataSource.getRepository(Produto);
+    }
+
     indIngredientByProdutoName(produto_name: string): Promise<Ingredient> {
         throw new Error("Method not implemented.");
     }
