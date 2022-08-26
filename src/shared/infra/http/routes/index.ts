9243a2d86776ff6e1738cc762fb935f02fd1dc5d
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-26 16:12:15 -0300

    add rotas de produto ao gerenciador de rotas

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { authorRoutes } from "./author.routes";
 import { emailRoutes } from "./email.routes";
 import { ingredientRoutes } from "./ingredient.routes";
+import { produtoRoutes } from "./produto.routes";
 import { recipeRoutes } from "./recipe.routes";
 
 export const routes = Router();
@@ -11,3 +12,4 @@ routes.use("/recipe", recipeRoutes);
 routes.use("/ingredient", ingredientRoutes);
 routes.use("/email", emailRoutes);
 routes.use("/author", authorRoutes);
+routes.use("produto", produtoRoutes);
