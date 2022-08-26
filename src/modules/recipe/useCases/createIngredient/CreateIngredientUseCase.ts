1f0ea2706871a48f3b0cbd5bcf1f89ab1a98f741
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-26 16:49:40 -0300

    creat instancia de produto p/ validar produto_name

--- src/modules/recipe/useCases/createIngredient/CreateIngredientUseCase.ts
+++ src/modules/recipe/useCases/createIngredient/CreateIngredientUseCase.ts
@@ -1,13 +1,19 @@
 import { ICreateIngredientDTO } from "@modules/recipe/dtos/ICreateIngredientDTO";
 import { Ingredient } from "@modules/recipe/infra/typeorm/entities/Ingredient";
 import { IIngredientRepository } from "@modules/recipe/repositories/IIngredientRepository";
+import { IProdutoRepository } from "@modules/recipe/repositories/IProdutoRepository";
 import { inject, injectable } from "tsyringe";
+
+import { AppError } from "@shared/errors/AppError";
 
 @injectable()
 export class CreateIngredientUseCase {
     constructor(
         @inject("IngredientRepository")
-        private ingredientRepository: IIngredientRepository
+        private ingredientRepository: IIngredientRepository,
+
+        @inject("ProdutoRepository")
+        private produtoRepository: IProdutoRepository
     ) {}
 
     async execute({
@@ -17,6 +23,14 @@ export class CreateIngredientUseCase {
         unity,
         weight,
     }: ICreateIngredientDTO): Promise<Ingredient> {
+        const produtoValidator = await this.produtoRepository.findProdutoByName(
+            produto_name
+        );
+
+        if (!produtoValidator) {
+            throw new AppError("Produto not found.", 401);
+        }
+
         const ingredient = await this.ingredientRepository.create({
             id,
             produto_name,
