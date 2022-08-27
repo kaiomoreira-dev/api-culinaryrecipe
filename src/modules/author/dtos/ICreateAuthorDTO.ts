b5a7d3d8abca6e8f203923af7a20799aa273eff6
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-27 15:29:28 -0300

    add atributos emails e recipes

--- src/modules/author/dtos/ICreateAuthorDTO.ts
+++ src/modules/author/dtos/ICreateAuthorDTO.ts
@@ -1,7 +1,15 @@
+import { Recipe } from "@modules/recipe/infra/typeorm/entities/Recipe";
+
+import { Email } from "../infra/typeorm/entities/Email";
+
 export interface ICreateAuthorDTO {
     id?: string;
 
     name: string;
 
     whatsapp: string;
+
+    emails: Email[];
+
+    recipes: Recipe[];
 }
