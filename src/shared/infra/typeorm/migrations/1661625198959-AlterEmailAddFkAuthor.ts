e0428ef68a2bdac72a9f780e248ec904b10349db
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-27 16:19:56 -0300

    create fk de author em email

---
+++ src/shared/infra/typeorm/migrations/1661625198959-AlterEmailAddFkAuthor.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from "typeorm";
+
+export class AlterEmailAddFkAuthor1661625198959 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      "emails",
+      new TableForeignKey({
+        name: "FKAuthorEmail",
+        referencedTableName: "authors",
+        referencedColumnNames: ["name"],
+        columnNames: ["author_name"],
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey("emails", "FKAuthorEmail");
+  }
+}
