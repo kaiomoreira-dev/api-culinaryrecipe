c07f9736abd3dc11ce265570ed3c41c7f24867ce
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-26 22:08:01 -0300

    corrigindo alterações incorentes

--- src/modules/recipe/repositories/IIngredientRepository.ts
+++ src/modules/recipe/repositories/IIngredientRepository.ts
@@ -1,16 +1,20 @@
 import { ICreateIngredientDTO } from "../dtos/ICreateIngredientDTO";
 import { Ingredient } from "../infra/typeorm/entities/Ingredient";
+import { Produto } from "../infra/typeorm/entities/Produto";
 
 export interface IIngredientRepository {
     create(data: ICreateIngredientDTO): Promise<Ingredient>;
     list(): Promise<Ingredient[]>;
+
     findIngredientByProdutoName(produto_name: string): Promise<Ingredient>;
     findAllIngredientByProdutoName(produto_name: string): Promise<Ingredient[]>;
+
     updateIngredientById(
         id: string,
         produto_name?: string,
         weight?: number,
         unity?: number
     ): Promise<Ingredient>;
+
     deleteIngredientById(id: string): Promise<void>;
 }
