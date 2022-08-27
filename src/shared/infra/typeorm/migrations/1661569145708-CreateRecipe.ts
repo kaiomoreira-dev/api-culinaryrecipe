f7f78d82dff016000ef68783f3a3631af0319e78
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-27 00:47:01 -0300

    create migration p/ criar recipe

---
+++ src/shared/infra/typeorm/migrations/1661569145708-CreateRecipe.ts
@@ -0,0 +1,72 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from "typeorm";
+
+export class CreateRecipe1661569145708 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "recipes",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+
+          {
+            name: "time",
+            type: "numeric",
+          },
+          {
+            name: "difficulty",
+            type: "varchar",
+            enum: ["easy", "medium", "hard"],
+          },
+          {
+            name: "dish_type",
+            type: "varchar",
+          },
+          {
+            name: "additional_features",
+            type: "varchar",
+          },
+          {
+            name: "total_guests",
+            type: "numeric",
+          },
+          {
+            name: "author_name",
+            type: "varchar",
+          },
+
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("recipes");
+  }
+}
