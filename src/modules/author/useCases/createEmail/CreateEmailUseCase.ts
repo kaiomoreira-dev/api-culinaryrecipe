7f0a32d96ffaf5d5e6e02094e117ee93bc6d87ac
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-27 16:15:40 -0300

    add atributo author_name em case de email

--- src/modules/author/useCases/createEmail/CreateEmailUseCase.ts
+++ src/modules/author/useCases/createEmail/CreateEmailUseCase.ts
@@ -1,5 +1,6 @@
 import { ICreateEmailDTO } from "@modules/author/dtos/ICreateEmailDTO";
 import { Email } from "@modules/author/infra/typeorm/entities/Email";
+import { IAuthorRepository } from "@modules/author/repositories/IAuthorRepository";
 import { IEmailRepository } from "@modules/author/repositories/IEmailRepository";
 import { inject, injectable } from "tsyringe";
 
@@ -9,10 +10,23 @@ import { AppError } from "@shared/errors/AppError";
 export class CreateEmailUseCase {
     constructor(
         @inject("EmailRepository")
-        private emailRepository: IEmailRepository
+        private emailRepository: IEmailRepository,
+        @inject("AuthorRepository")
+        private authorRepository: IAuthorRepository
     ) {}
 
-    async execute({ id, e_mail }: ICreateEmailDTO): Promise<Email> {
+    async execute({
+        id,
+        e_mail,
+        author_name,
+    }: ICreateEmailDTO): Promise<Email> {
+        const authorValidator = await this.authorRepository.findAuthorByName(
+            author_name
+        );
+        if (!authorValidator) {
+            throw new AppError("Author not found", 404);
+        }
+
         const emailValidator = await this.emailRepository.findEmailByE_mail(
             e_mail
         );
@@ -23,6 +37,7 @@ export class CreateEmailUseCase {
         const email = await this.emailRepository.create({
             id,
             e_mail,
+            author_name,
         });
 
         return email;
