0c1c700c1e21f1fe28a88d4c38763fd64da03e2d
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-27 15:22:15 -0300

    create rota get p/ retornar authors

--- src/shared/infra/http/routes/author.routes.ts
+++ src/shared/infra/http/routes/author.routes.ts
@@ -1,8 +1,13 @@
 import { CreateAuthorController } from "@modules/author/useCases/createAuthor/CreateAuthorController";
+import { ListAuthorController } from "@modules/author/useCases/listAuthor/ListAuthorController";
 import { Router } from "express";
 
 export const authorRoutes = Router();
 
 const createAuthorController = new CreateAuthorController();
 
+const listAuthorController = new ListAuthorController();
+
 authorRoutes.post("/", createAuthorController.handle);
+
+authorRoutes.get("/", listAuthorController.handle);
