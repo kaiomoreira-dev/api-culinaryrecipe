57638dcba22c7301c002b1fc815d7078c5f7bb82
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-27 00:47:28 -0300

    reorganizando sequencia de criação de migration

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -5,16 +5,14 @@ import { Produto } from "@modules/recipe/infra/typeorm/entities/Produto";
 import { Recipe } from "@modules/recipe/infra/typeorm/entities/Recipe";
 import { DataSource } from "typeorm";
 
-import { CreateRecipe1660942928513 } from "./migrations/1660942928513-CreateRecipe";
 import { CreateIngredient1661028123973 } from "./migrations/1661028123973-CreateIngredient";
 import { CreateRecipesIngredients1661042649597 } from "./migrations/1661042649597-CreateRecipesIngredients";
-import { AlterRecipeRemoveIngredient1661092158623 } from "./migrations/1661092158623-AlterRecipeRemoveIngredient";
 import { CreateEmail1661287166449 } from "./migrations/1661287166449-CreateEmail";
-import { AlterRecipeChangeAuthorForAuthorName1661385979738 } from "./migrations/1661385979738-AlterRecipeChangeAuthorForAuthorName";
 import { CreateAuthor1661395822193 } from "./migrations/1661395822193-CreateAuthor";
 import { CreateProduto1661479247977 } from "./migrations/1661479247977-CreateProduto";
 import { AlterIngredientsAddFKProdutos1661550373729 } from "./migrations/1661550373729-AlterIngredientsAddFKProdutos";
 import { AlterRecipeAddFKAuthor1661567666428 } from "./migrations/1661567666428-AlterRecipeAddFKAuthor";
+import { CreateRecipe1661569145708 } from "./migrations/1661569145708-CreateRecipe";
 
 const dataSource = new DataSource({
   type: "postgres",
@@ -27,18 +25,17 @@ const dataSource = new DataSource({
       : "culinary_recipes",
 
   // importar entidades ex: [Recipes]
-  entities: [Recipe, Produto, Ingredient, Email, Author],
+  entities: [Author, Recipe, Produto, Ingredient, Email],
   // importar migrations ex: [CreateRecipes102348998]
   migrations: [
-    CreateRecipe1660942928513,
     CreateProduto1661479247977,
-    CreateIngredient1661028123973,
-    CreateRecipesIngredients1661042649597,
-    AlterRecipeRemoveIngredient1661092158623,
-    CreateEmail1661287166449,
-    AlterRecipeChangeAuthorForAuthorName1661385979738,
     CreateAuthor1661395822193,
+    CreateIngredient1661028123973,
+    CreateEmail1661287166449,
     AlterIngredientsAddFKProdutos1661550373729,
+    CreateRecipe1661569145708,
+    CreateRecipesIngredients1661042649597,
+    AlterRecipeAddFKAuthor1661567666428,
   ],
 });
 
