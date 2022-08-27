848abe6791bc3b2ad31eb22626f3dce9d505e019
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-26 23:33:37 -0300

    remove importação de email e ingredient

--- src/modules/author/dtos/ICreateAuthorDTO.ts
+++ src/modules/author/dtos/ICreateAuthorDTO.ts
@@ -1,7 +1,3 @@
-import { Recipe } from "@modules/recipe/infra/typeorm/entities/Recipe";
-
-import { Email } from "../infra/typeorm/entities/Email";
-
 export interface ICreateAuthorDTO {
     id?: string;
 
