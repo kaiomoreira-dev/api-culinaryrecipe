ea7648cf832ec362ad07504620d8df390275fcd7
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-27 17:48:15 -0300

    create instancias p/ listar recipes por produto

---
+++ src/modules/recipe/useCases/recipesIngredients/listAllRecipesByIngredientProdutoName/ListAllRecipesByImgredientProdutoNameUseCase.ts
@@ -0,0 +1,14 @@
+import { IIngredientRepository } from "@modules/recipe/repositories/IIngredientRepository";
+import { IRecipeRepository } from "@modules/recipe/repositories/IRecipeRepository";
+import { inject, injectable } from "tsyringe";
+
+@injectable()
+export class ListAllRecipesByImgredientProdutoNameUseCase {
+    constructor(
+        @inject("RecipeRepository")
+        private recipeRepository: IRecipeRepository,
+
+        @inject("IngredientRepository")
+        private ingredientRepository: IIngredientRepository
+    ) {}
+}
