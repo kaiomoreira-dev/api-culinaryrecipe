9add81b2f45755e98823accc2514f25127f879f1
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-27 21:25:50 -0300

    create metodo p/ retornar recipe p/ produto name

--- src/modules/recipe/repositories/IRecipeRepository.ts
+++ src/modules/recipe/repositories/IRecipeRepository.ts
@@ -4,6 +4,9 @@ import { Recipe } from "../infra/typeorm/entities/Recipe";
 export interface IRecipeRepository {
     create(data: ICreateRecipeDTO): Promise<Recipe>;
     list(): Promise<Recipe[]>;
+    listAllRecipeByIngredientProdutoName(
+        produto_name: string
+    ): Promise<Recipe[]>;
 
     findRecipeById(id: string): Promise<Recipe>;
     findRecipesByDifficulty(difficulty: string): Promise<Recipe[]>;
