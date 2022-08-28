2218270f785ad3fe7fdbdec3ee9f064558cc87b7
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-27 21:27:11 -0300

    create case p/ retornar recipes por ingredient

--- src/modules/recipe/useCases/recipesIngredients/listAllRecipesByIngredientProdutoName/ListAllRecipesByImgredientProdutoNameUseCase.ts
+++ src/modules/recipe/useCases/recipesIngredients/listAllRecipesByIngredientProdutoName/ListAllRecipesByImgredientProdutoNameUseCase.ts
@@ -2,6 +2,8 @@ import { IIngredientRepository } from "@modules/recipe/repositories/IIngredientR
 import { IRecipeRepository } from "@modules/recipe/repositories/IRecipeRepository";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "@shared/errors/AppError";
+
 @injectable()
 export class ListAllRecipesByImgredientProdutoNameUseCase {
     constructor(
@@ -11,4 +13,22 @@ export class ListAllRecipesByImgredientProdutoNameUseCase {
         @inject("IngredientRepository")
         private ingredientRepository: IIngredientRepository
     ) {}
+
+    async execute(produto_name: string): Promise<any> {
+        const produtoValidator =
+            await this.ingredientRepository.findAllIngredientByProdutoName(
+                produto_name
+            );
+
+        if (!produtoValidator) {
+            throw new AppError("Product not found", 404);
+        }
+
+        const recipes =
+            await this.recipeRepository.listAllRecipeByIngredientProdutoName(
+                produto_name
+            );
+
+        return recipes;
+    }
 }
