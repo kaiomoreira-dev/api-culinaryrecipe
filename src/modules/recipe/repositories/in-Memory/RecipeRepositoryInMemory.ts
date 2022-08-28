8444453294aa1a7cdbe5af8ec06acb87d31ff17d
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-28 16:28:00 -0300

    creata atributo repository p/ usar serviços do orm

---
+++ src/modules/recipe/repositories/in-Memory/RecipeRepositoryInMemory.ts
@@ -0,0 +1,54 @@
+import { ICreateRecipeDTO } from "@modules/recipe/dtos/ICreateRecipeDTO";
+import { Recipe } from "@modules/recipe/infra/typeorm/entities/Recipe";
+import { inject, injectable } from "tsyringe";
+import { Repository } from "typeorm";
+
+import dataSource from "@shared/infra/typeorm";
+
+import { IIngredientRepository } from "../IIngredientRepository";
+import { IRecipeRepository } from "../IRecipeRepository";
+
+@injectable()
+export class RecipeRepositoryInMemory implements IRecipeRepository {
+    private repository: Repository<Recipe>;
+
+    constructor() {
+        this.repository = dataSource.getRepository(Recipe);
+    }
+
+    create(data: ICreateRecipeDTO): Promise<Recipe> {
+        throw new Error("Method not implemented.");
+    }
+    list(): Promise<Recipe[]> {
+        throw new Error("Method not implemented.");
+    }
+    listAllRecipeByIngredientProdutoName(
+        produto_name: string
+    ): Promise<Recipe[]> {
+        throw new Error("Method not implemented.");
+    }
+    findRecipeById(id: string): Promise<Recipe> {
+        throw new Error("Method not implemented.");
+    }
+    findRecipesByDifficulty(difficulty: string): Promise<Recipe[]> {
+        throw new Error("Method not implemented.");
+    }
+    findRecipeByAuthor(author_name: string): Promise<Recipe> {
+        throw new Error("Method not implemented.");
+    }
+    findRecipeByName(name: string): Promise<Recipe> {
+        throw new Error("Method not implemented.");
+    }
+    updateTimeByRecipeId(id: string, time: number): Promise<Recipe> {
+        throw new Error("Method not implemented.");
+    }
+    updateAuthorNameByRecipeId(
+        id: string,
+        author_name: string
+    ): Promise<Recipe> {
+        throw new Error("Method not implemented.");
+    }
+    deleteRecipeById(id: string): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+}
