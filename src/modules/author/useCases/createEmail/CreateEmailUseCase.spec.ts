9f8e07e6fdcdbf3b25aea135702adc5ad936b806
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-28 16:20:09 -0300

    create repo de recipes e implement sua interface

--- src/modules/author/useCases/createEmail/CreateEmailUseCase.spec.ts
+++ src/modules/author/useCases/createEmail/CreateEmailUseCase.spec.ts
@@ -1,24 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { faker } from "@faker-js/faker";
 import { ICreateEmailDTO } from "@modules/author/dtos/ICreateEmailDTO";
+import { AuthorRepositoryInMemory } from "@modules/author/repositories/in-Memory/AuthorRepositoryInMemory";
 import { EmailRepositoryInMemory } from "@modules/author/repositories/in-Memory/EmailRepositoryInMemory";
 
 import { AppError } from "@shared/errors/AppError";
 
+import { CreateAuthorUseCase } from "../createAuthor/CreateAuthorUseCase";
 import { CreateEmailUseCase } from "./CreateEmailUseCase";
 
 let emailRepositoryInMemory: EmailRepositoryInMemory;
+let authorRepositoryInMemory: AuthorRepositoryInMemory;
+let createAuthorUseCase: CreateAuthorUseCase;
 let createEmailUseCase: CreateEmailUseCase;
 
 describe("Create e-mail UseCase", () => {
     beforeEach(() => {
         emailRepositoryInMemory = new EmailRepositoryInMemory();
-        createEmailUseCase = new CreateEmailUseCase(emailRepositoryInMemory);
+        authorRepositoryInMemory = new AuthorRepositoryInMemory(
+            emailRepositoryInMemory
+        );
+        createAuthorUseCase = new CreateAuthorUseCase(authorRepositoryInMemory);
+        createEmailUseCase = new CreateEmailUseCase(
+            emailRepositoryInMemory,
+            authorRepositoryInMemory
+        );
     });
 
     it("should be able to create a new e-mail", async () => {
         const email: ICreateEmailDTO = {
             id: faker.datatype.uuid(),
             e_mail: faker.internet.email(),
+            author_name: faker.name.fullName(),
         };
 
         const e_mail = await createEmailUseCase.execute(email);
@@ -32,11 +45,13 @@ describe("Create e-mail UseCase", () => {
         const email1: ICreateEmailDTO = {
             id: faker.datatype.uuid(),
             e_mail: "test@test.com.br",
+            author_name: faker.name.fullName(),
         };
 
         const email2: ICreateEmailDTO = {
             id: faker.datatype.uuid(),
             e_mail: "test@test.com.br",
+            author_name: faker.name.fullName(),
         };
 
         const e_mail1 = await createEmailUseCase.execute(email1);
