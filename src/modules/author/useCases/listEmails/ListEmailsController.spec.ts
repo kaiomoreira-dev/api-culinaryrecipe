e64a8f4b736f18d8e836134a2d1e362b37250af7
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-28 23:19:10 -0300

    add author p/ criar email c author_name controller

--- src/modules/author/useCases/listEmails/ListEmailsController.spec.ts
+++ src/modules/author/useCases/listEmails/ListEmailsController.spec.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import { faker } from "@faker-js/faker";
+import { Author } from "@modules/author/infra/typeorm/entities/Author";
 import request from "supertest";
 import { DataSource } from "typeorm";
 
@@ -22,19 +23,30 @@ describe("List e-mails Controller", () => {
     });
 
     it("should be able to list e-mails", async () => {
+        const responseAuthor = await request(app).post("/author").send({
+            id: faker.datatype.uuid(),
+            name: faker.name.fullName(),
+            whatsapp: faker.phone.number(),
+        });
+
+        const author = responseAuthor.body as Author;
+
         await request(app).post("/email").send({
             id: faker.datatype.uuid(),
             e_mail: faker.internet.email(),
+            author_name: author.name,
         });
 
         await request(app).post("/email").send({
             id: faker.datatype.uuid(),
             e_mail: faker.internet.email(),
+            author_name: author.name,
         });
 
         await request(app).post("/email").send({
             id: faker.datatype.uuid(),
             e_mail: faker.internet.email(),
+            author_name: author.name,
         });
 
         const response = await request(app).get("/email");
