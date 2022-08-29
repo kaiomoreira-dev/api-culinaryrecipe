2296fee8431424ead0d6c93473572839b0f8e70b
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-28 23:05:00 -0300

    remove criação de entidade recipes-ingredients

--- src/shared/infra/typeorm/migrations/1661569145708-CreateRecipe.ts
+++ src/shared/infra/typeorm/migrations/1661569145708-CreateRecipe.ts
@@ -70,50 +70,6 @@ export class CreateRecipe1661569145708 implements MigrationInterface {
         ],
       })
     );
-
-    await queryRunner.createTable(
-      new Table({
-        name: "recipes_ingredients",
-        columns: [
-          {
-            name: "recipe_id",
-            type: "uuid",
-          },
-          {
-            name: "ingredient_id",
-            type: "uuid",
-          },
-          {
-            name: "created_at",
-            type: "timestamp",
-            default: "now()",
-          },
-          {
-            name: "updated_at",
-            type: "timestamp",
-            default: "now()",
-          },
-        ],
-        foreignKeys: [
-          {
-            name: "FKRecipeIngredient",
-            referencedTableName: "recipes",
-            referencedColumnNames: ["id"],
-            columnNames: ["recipe_id"],
-            onDelete: "SET NULL",
-            onUpdate: "SET NULL",
-          },
-          {
-            name: "FKIngredientRecipe",
-            referencedTableName: "ingredients",
-            referencedColumnNames: ["id"],
-            columnNames: ["ingredient_id"],
-            onDelete: "SET NULL",
-            onUpdate: "SET NULL",
-          },
-        ],
-      })
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
