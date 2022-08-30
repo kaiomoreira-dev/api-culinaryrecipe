bb15ece1b252d65ed02e12ff9f63b9417a866445
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-30 13:43:21 -0300

    create metodo delete p/ deletar email no repo

--- src/modules/author/repositories/in-Memory/EmailRepositoryInMemory.ts
+++ src/modules/author/repositories/in-Memory/EmailRepositoryInMemory.ts
@@ -6,6 +6,14 @@ import { IEmailRepository } from "../IEmailRepository";
 export class EmailRepositoryInMemory implements IEmailRepository {
     private emailsRepository: Email[] = [];
 
+    async deleteEmailByEmail(e_mail: string): Promise<void> {
+        const emailIndex = this.emailsRepository.findIndex(
+            (email) => email.e_mail === e_mail
+        );
+
+        this.emailsRepository.splice(emailIndex, 1);
+    }
+
     async create({ id, e_mail, author_name }: ICreateEmailDTO): Promise<Email> {
         const email = new Email();
 
