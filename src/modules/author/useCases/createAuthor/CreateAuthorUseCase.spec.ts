a3c11fb2af72ba52348138c0ba17e195eba40542
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-30 17:18:29 -0300

    import e instance recipe e email in memory

--- src/modules/author/useCases/createAuthor/CreateAuthorUseCase.spec.ts
+++ src/modules/author/useCases/createAuthor/CreateAuthorUseCase.spec.ts
@@ -1,8 +1,20 @@
 import { AuthorRepositoryInMemory } from "@modules/author/repositories/in-Memory/AuthorRepositoryInMemory";
+import { EmailRepositoryInMemory } from "@modules/author/repositories/in-Memory/EmailRepositoryInMemory";
+import { RecipeRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/RecipeRepositoryInMemory";
 
 import { CreateAuthorUseCase } from "./CreateAuthorUseCase";
 
+let emailRepositoryInMemory: EmailRepositoryInMemory;
+let recipeRepositoryInMemory: RecipeRepositoryInMemory;
 let authorRepositoryInMemory: AuthorRepositoryInMemory;
 let createAuthorUseCase: CreateAuthorUseCase;
 
-describe("Create author UseCase", () => {});
+describe("Create author UseCase", () => {
+    emailRepositoryInMemory = new EmailRepositoryInMemory();
+    recipeRepositoryInMemory = new RecipeRepositoryInMemory();
+    authorRepositoryInMemory = new AuthorRepositoryInMemory(
+        emailRepositoryInMemory,
+        recipeRepositoryInMemory
+    );
+    createAuthorUseCase = new CreateAuthorUseCase(authorRepositoryInMemory);
+});
