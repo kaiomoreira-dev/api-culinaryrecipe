a061b3a038ab8369244c8870cb8bd841a4208454
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-30 13:33:09 -0300

    implement handle p/ deletar email c/ status 200

--- src/modules/author/useCases/deleteEmail/DeleteEmailController.ts
+++ src/modules/author/useCases/deleteEmail/DeleteEmailController.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { DeleteEmailUseCase } from "./DeleteEmailUseCase";
 
 export class DeleteEmailController {
-    async handle(request: Request, response: Response): Promise<Response> {}
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { e_mail } = request.body;
+
+        const deleteEmailUseCase = container.resolve(DeleteEmailUseCase);
+
+        await deleteEmailUseCase.execute(e_mail);
+
+        return response
+            .status(200)
+            .json({ message: "Email deleted successfully." });
+    }
 }
