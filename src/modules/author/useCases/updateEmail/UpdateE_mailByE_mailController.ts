3f2aa373920f174f39ee9ffada6abacc90178751
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-30 15:22:57 -0300

    alter controler adding newE_mail, oldE_mail

--- src/modules/author/useCases/updateEmail/UpdateE_mailByE_mailController.ts
+++ src/modules/author/useCases/updateEmail/UpdateE_mailByE_mailController.ts
@@ -5,13 +5,13 @@ import { UpdateE_mailByE_mailUseCase } from "./UpdateEmailByE_mailUseCase";
 
 export class UpdateE_mailByE_mailController {
     async handle(request: Request, response: Response): Promise<Response> {
-        const { e_mail } = request.body;
+        const { oldE_mail, newE_mail } = request.body;
 
         const updateE_mailByE_mailUseCase = container.resolve(
             UpdateE_mailByE_mailUseCase
         );
 
-        const email = updateE_mailByE_mailUseCase.execute(e_mail);
+        const email = updateE_mailByE_mailUseCase.execute(oldE_mail, newE_mail);
 
         return response.status(200).json(email);
     }
