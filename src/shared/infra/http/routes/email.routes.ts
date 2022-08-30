6744ab7f92b7fb064a838462bff5cdf4c8dce523
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-30 13:37:37 -0300

    crete rota delete p/ deletar email

--- src/shared/infra/http/routes/email.routes.ts
+++ src/shared/infra/http/routes/email.routes.ts
@@ -1,4 +1,5 @@
 import { CreateEmailController } from "@modules/author/useCases/createEmail/CreateEmailController";
+import { DeleteEmailController } from "@modules/author/useCases/deleteEmail/DeleteEmailController";
 import { FindEmailController } from "@modules/author/useCases/findEmail/FindEmailController";
 import { ListEmailsController } from "@modules/author/useCases/listEmails/ListEmailsController";
 import { Router } from "express";
@@ -11,8 +12,12 @@ const listEmailsController = new ListEmailsController();
 
 const findEmailController = new FindEmailController();
 
+const deleteEmailController = new DeleteEmailController();
+
 emailRoutes.post("/", createEmailController.handle);
 
 emailRoutes.get("/", listEmailsController.handle);
 
 emailRoutes.get("/find", findEmailController.handle);
+
+emailRoutes.delete("/delete", deleteEmailController.handle);
