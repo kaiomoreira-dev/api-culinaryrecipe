5fdf161cdabb352772528b2c91a26ac0dc19cfeb
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-29 21:31:44 -0300

    validando email e retornando objeto email inteiro

--- src/modules/author/useCases/findEmail/FindEmailUseCase.ts
+++ src/modules/author/useCases/findEmail/FindEmailUseCase.ts
@@ -1,6 +1,26 @@
-import { injectable } from "tsyringe";
+import { ICreateEmailDTO } from "@modules/author/dtos/ICreateEmailDTO";
+import { Email } from "@modules/author/infra/typeorm/entities/Email";
+import { IEmailRepository } from "@modules/author/repositories/IEmailRepository";
+import { inject, injectable } from "tsyringe";
+
+import { AppError } from "@shared/errors/AppError";
 
 @injectable()
 export class FindEmailUseCase {
-    constructor() {}
+    constructor(
+        @inject("EmailRepository")
+        private emailRepository: IEmailRepository
+    ) {}
+
+    async execute({ e_mail }: ICreateEmailDTO): Promise<Email> {
+        const emailValidator = await this.emailRepository.findEmailByE_mail(
+            e_mail
+        );
+
+        if (!emailValidator) {
+            throw new AppError("Email not found", 404);
+        }
+
+        return emailValidator;
+    }
 }
