7ff6740fac3f8578c9616d22679e120dd72f2385
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-30 14:03:58 -0300

    importando dataSource, faker outras config p/ test

---
+++ src/modules/author/useCases/deleteEmail/DeleteEmailController.spec.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import { faker } from "@faker-js/faker";
+import { Author } from "@modules/author/infra/typeorm/entities/Author";
+import request from "supertest";
+import { DataSource } from "typeorm";
+
+import { app } from "@shared/infra/http/app";
+import { createConnection } from "@shared/infra/typeorm";
+
+let connection: DataSource;
