a88cf6035905cc636071f1a3573bffcb28d8c0a4
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-29 22:13:40 -0300

    create test p/ encontrar email

---
+++ src/modules/author/useCases/findEmail/FindEmailUseCase.spec.ts
@@ -0,0 +1,59 @@
+import { faker } from "@faker-js/faker";
+import { ICreateAuthorDTO } from "@modules/author/dtos/ICreateAuthorDTO";
+import { ICreateEmailDTO } from "@modules/author/dtos/ICreateEmailDTO";
+import { AuthorRepositoryInMemory } from "@modules/author/repositories/in-Memory/AuthorRepositoryInMemory";
+import { EmailRepositoryInMemory } from "@modules/author/repositories/in-Memory/EmailRepositoryInMemory";
+import { RecipeRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/RecipeRepositoryInMemory";
+
+import { AppError } from "@shared/errors/AppError";
+
+import { CreateAuthorUseCase } from "../createAuthor/CreateAuthorUseCase";
+import { CreateEmailUseCase } from "../createEmail/CreateEmailUseCase";
+import { FindEmailUseCase } from "./FindEmailUseCase";
+
+let emailRepositoryInMemory: EmailRepositoryInMemory;
+let authorRepositoryInMemory: AuthorRepositoryInMemory;
+let recipeRepositoryInMemory: RecipeRepositoryInMemory;
+let createAuthorUseCase: CreateAuthorUseCase;
+let createEmailUseCase: CreateEmailUseCase;
+let findEmailUseCase: FindEmailUseCase;
+
+describe("Find email UseCase", () => {
+    beforeEach(() => {
+        emailRepositoryInMemory = new EmailRepositoryInMemory();
+        recipeRepositoryInMemory = new RecipeRepositoryInMemory();
+        authorRepositoryInMemory = new AuthorRepositoryInMemory(
+            emailRepositoryInMemory,
+            recipeRepositoryInMemory
+        );
+        createAuthorUseCase = new CreateAuthorUseCase(authorRepositoryInMemory);
+        createEmailUseCase = new CreateEmailUseCase(
+            emailRepositoryInMemory,
+            authorRepositoryInMemory
+        );
+        findEmailUseCase = new FindEmailUseCase(emailRepositoryInMemory);
+    });
+
+    it("should be able to find email", async () => {
+        const author: ICreateAuthorDTO = {
+            id: faker.datatype.uuid(),
+            name: faker.name.fullName(),
+            whatsapp: faker.phone.number(),
+        };
+
+        const authorCreated = await createAuthorUseCase.execute(author);
+
+        const email: ICreateEmailDTO = {
+            id: faker.datatype.uuid(),
+            e_mail: "test@test.com.br",
+            author_name: authorCreated.name,
+        };
+
+        await createEmailUseCase.execute(email);
+
+        const foundEmail = await findEmailUseCase.execute(email.e_mail);
+
+        expect(foundEmail).toHaveProperty("id");
+        expect(foundEmail).toHaveProperty("e_mail");
+    });
+});
