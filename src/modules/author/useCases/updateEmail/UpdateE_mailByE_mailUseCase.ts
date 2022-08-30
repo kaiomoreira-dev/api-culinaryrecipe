cbed4b1c45be88f49e1f9aa631c57bc448d5b27e
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-30 16:07:25 -0300

    alter name do file p/ UpdateE_mailByE_mailUseCase

---
+++ src/modules/author/useCases/updateEmail/UpdateE_mailByE_mailUseCase.ts
@@ -0,0 +1,40 @@
+import { Email } from "@modules/author/infra/typeorm/entities/Email";
+import { IEmailRepository } from "@modules/author/repositories/IEmailRepository";
+import { inject, injectable } from "tsyringe";
+
+import { AppError } from "@shared/errors/AppError";
+
+@injectable()
+export class UpdateE_mailByE_mailUseCase {
+    constructor(
+        @inject("EmailRepository")
+        private emailRepository: IEmailRepository
+    ) {}
+
+    async execute(oldE_mail: string, newE_mail: string): Promise<Email> {
+        const emailValidator = await this.emailRepository.findEmailByE_mail(
+            oldE_mail
+        );
+
+        if (!emailValidator) {
+            throw new AppError("Email not found", 404);
+        }
+
+        const newEmailValidator = await this.emailRepository.findEmailByE_mail(
+            newE_mail
+        );
+
+        if (newEmailValidator) {
+            throw new AppError("Email already exists", 401);
+        }
+
+        const oldE_mailFormat = emailValidator.e_mail;
+
+        const email = await this.emailRepository.updateE_mailByE_mail(
+            oldE_mailFormat,
+            newE_mail
+        );
+
+        return email;
+    }
+}
