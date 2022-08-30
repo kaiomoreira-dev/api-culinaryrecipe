dd475484291ed211f8f440a6315dd2f059e48744
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-30 15:07:13 -0300

    alter name do controller p/ UpdateE_mailByEmail

---
+++ src/modules/author/useCases/updateEmail/UpdateE_mailByE_mailController.ts
@@ -0,0 +1,18 @@
+import e, { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { UpdateE_mailByE_mailUseCase } from "./UpdateEmailByE_mailUseCase";
+
+export class UpdateE_mailByE_mailController {
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { e_mail } = request.body;
+
+        const updateE_mailByE_mailUseCase = container.resolve(
+            UpdateE_mailByE_mailUseCase
+        );
+
+        const email = updateE_mailByE_mailUseCase.execute(e_mail);
+
+        return response.status(200).json(email);
+    }
+}
