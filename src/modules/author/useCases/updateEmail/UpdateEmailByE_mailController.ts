4ea335b2970a4755c7b825b12eef2461bc09d303
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-30 15:00:51 -0300

    implement handle p/ atualzaiar e_mail c/ e_mail

--- src/modules/author/useCases/updateEmail/UpdateEmailByE_mailController.ts
+++ src/modules/author/useCases/updateEmail/UpdateEmailByE_mailController.ts
@@ -1,5 +1,18 @@
-import { Request, Response } from "express";
+import e, { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { UpdateEmailByE_mailUseCase } from "./UpdateEmailByE_mailUseCase";
 
 export class UpdateEmailByE_mailController {
-    async handle(request: Request, response: Response): Promise<Response> {}
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { e_mail } = request.body;
+
+        const updateE_mailByE_mail = container.resolve(
+            UpdateEmailByE_mailUseCase
+        );
+
+        const email = updateE_mailByE_mail.execute(e_mail);
+
+        return response.status(200).json(email);
+    }
 }
