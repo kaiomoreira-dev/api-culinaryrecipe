6ec9365c83d013d71c6a59d7be9997b920c9694d
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-30 16:32:28 -0300

    create test p/ valdiar newE_mail se ja existe

--- src/modules/author/useCases/updateEmail/UpdateE_mailByE_mailUseCase.spec.ts
+++ src/modules/author/useCases/updateEmail/UpdateE_mailByE_mailUseCase.spec.ts
@@ -5,6 +5,8 @@ import { AuthorRepositoryInMemory } from "@modules/author/repositories/in-Memory
 import { EmailRepositoryInMemory } from "@modules/author/repositories/in-Memory/EmailRepositoryInMemory";
 import { RecipeRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/RecipeRepositoryInMemory";
 
+import { AppError } from "@shared/errors/AppError";
+
 import { CreateAuthorUseCase } from "../createAuthor/CreateAuthorUseCase";
 import { CreateEmailUseCase } from "../createEmail/CreateEmailUseCase";
 import { UpdateE_mailByE_mailUseCase } from "./UpdateE_mailByE_mailUseCase";
@@ -61,9 +63,50 @@ describe("Update e-mail UseCase", () => {
             newE_mail
         );
 
-        console.log(updateE_mail);
-
         expect(updateE_mail).toHaveProperty("id");
         expect(updateE_mail).toHaveProperty("e_mail");
+    });
+
+    it("should not be able to update e_mail whit oldE_mail invalid", async () => {
+        const oldE_mail = "fake-old-email@email.com";
+
+        const newE_mail = "new-email@email.com";
+
+        await expect(
+            updateE_mailByE_mailUseCase.execute(oldE_mail, newE_mail)
+        ).rejects.toEqual(new AppError("Email not found", 404));
+    });
+
+    it("should not be able to update e_mail with newE_mail already exists", async () => {
+        const author: ICreateAuthorDTO = {
+            id: faker.datatype.uuid(),
+            name: faker.name.fullName(),
+            whatsapp: faker.phone.number(),
+        };
+
+        const authorCreated = await createAuthorUseCase.execute(author);
+
+        const email1: ICreateEmailDTO = {
+            id: faker.datatype.uuid(),
+            e_mail: "old1-email@email.com",
+            author_name: authorCreated.name,
+        };
+
+        const email2: ICreateEmailDTO = {
+            id: faker.datatype.uuid(),
+            e_mail: "old2-email@email.com",
+            author_name: authorCreated.name,
+        };
+
+        const { e_mail } = await createEmailUseCase.execute(email1);
+        await createEmailUseCase.execute(email2);
+
+        const oldE_mail = e_mail;
+
+        const newE_mail = "old2-email@email.com";
+
+        await expect(
+            updateE_mailByE_mailUseCase.execute(oldE_mail, newE_mail)
+        ).rejects.toEqual(new AppError("Email already exists", 401));
     });
 });
