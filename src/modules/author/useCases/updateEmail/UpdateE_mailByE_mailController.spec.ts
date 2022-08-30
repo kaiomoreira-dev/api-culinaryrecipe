d8d0e40b125313e9e54ea2b93248b84b9561b597
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-30 16:20:17 -0300

    create test p/ atualizar email por email

--- src/modules/author/useCases/updateEmail/UpdateE_mailByE_mailController.spec.ts
+++ src/modules/author/useCases/updateEmail/UpdateE_mailByE_mailController.spec.ts
@@ -1,5 +1,7 @@
 import "dotenv/config";
 import { faker } from "@faker-js/faker";
+import { Author } from "@modules/author/infra/typeorm/entities/Author";
+import { Email } from "@modules/author/infra/typeorm/entities/Email";
 import request from "supertest";
 import { DataSource } from "typeorm";
 
@@ -19,5 +21,32 @@ describe("Update e-mail Controller", () => {
         await connection.dropDatabase();
 
         await connection.destroy();
+    });
+
+    it("should be able to update e_mail with e_mail", async () => {
+        const responseAuthor = await request(app).post("/author").send({
+            id: faker.datatype.uuid(),
+            name: faker.name.fullName(),
+            whatsapp: faker.phone.number(),
+        });
+
+        const author = responseAuthor.body as Author;
+
+        const responseCreateEmail = await request(app).post("/email").send({
+            id: faker.datatype.uuid(),
+            e_mail: "old-email@email.com",
+            author_name: author.name,
+        });
+
+        const { e_mail } = responseCreateEmail.body as Email;
+
+        const oldE_mail = e_mail;
+        const newE_mail = "new-email@email.com";
+
+        const responseUpdateE_mail = await request(app)
+            .patch("/email/update")
+            .send({ oldE_mail, newE_mail });
+
+        expect(responseUpdateE_mail.status).toBe(200);
     });
 });
