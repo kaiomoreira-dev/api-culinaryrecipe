1b051a6cecb3bb288aad547b146b56d4f3abedd7
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-30 14:26:20 -0300

    create test p/ validar email antes de deletar

--- src/modules/author/useCases/deleteEmail/DeleteEmailController.spec.ts
+++ src/modules/author/useCases/deleteEmail/DeleteEmailController.spec.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { faker } from "@faker-js/faker";
 import { Author } from "@modules/author/infra/typeorm/entities/Author";
+import { Email } from "@modules/author/infra/typeorm/entities/Email";
 import request from "supertest";
 import { DataSource } from "typeorm";
 
@@ -20,5 +21,43 @@ describe("Delete e-mail Controller", () => {
         await connection.dropDatabase();
 
         await connection.destroy();
+    });
+
+    it("should be able to delete an e-mail", async () => {
+        const responseAuthor = await request(app).post("/author").send({
+            id: faker.datatype.uuid(),
+            name: faker.name.fullName(),
+            whatsapp: faker.phone.number(),
+        });
+
+        const author = responseAuthor.body as Author;
+
+        const responseCreateEmail = await request(app).post("/email").send({
+            id: faker.datatype.uuid(),
+            e_mail: faker.internet.email(),
+            author_name: author.name,
+        });
+
+        const { e_mail } = responseCreateEmail.body as Email;
+
+        const responseDeleteEmail = await request(app)
+            .delete("/email/delete")
+            .send({
+                e_mail,
+            });
+
+        expect(responseDeleteEmail.statusCode).toBe(200);
+    });
+
+    it("should not be to delete email with invalid email", async () => {
+        const e_mail = "fake-email@fake.com";
+
+        const responseDeleteEmail = await request(app)
+            .delete("/email/delete")
+            .send({
+                e_mail,
+            });
+
+        expect(responseDeleteEmail.statusCode).toBe(404);
     });
 });
