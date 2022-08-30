7ce829a49d7265180fa3f41ff57c0832584c968c
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-29 22:23:46 -0300

    create test controller p/ retornar email buscado

---
+++ src/modules/author/useCases/findEmail/FindEmailController.spec.ts
@@ -0,0 +1,50 @@
+import "dotenv/config";
+import { faker } from "@faker-js/faker";
+import { ICreateAuthorDTO } from "@modules/author/dtos/ICreateAuthorDTO";
+import { Author } from "@modules/author/infra/typeorm/entities/Author";
+import { Email } from "@modules/author/infra/typeorm/entities/Email";
+import request from "supertest";
+import { DataSource } from "typeorm";
+
+import { app } from "@shared/infra/http/app";
+import { createConnection } from "@shared/infra/typeorm";
+
+let connection: DataSource;
+
+describe("Find e-mail Controller", () => {
+    beforeAll(async () => {
+        connection = await createConnection("localhost");
+
+        await connection.runMigrations();
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+
+        await connection.destroy();
+    });
+
+    it("should be able to find email", async () => {
+        const responseCreateAuthor = await request(app).post("/author").send({
+            id: faker.datatype.uuid(),
+            name: faker.name.fullName(),
+            whatsapp: faker.phone.number(),
+        });
+
+        const author = responseCreateAuthor.body as Author;
+
+        const responseCreateEmail = await request(app).post("/email").send({
+            id: faker.datatype.uuid(),
+            e_mail: faker.internet.email(),
+            author_name: author.name,
+        });
+
+        const { e_mail } = responseCreateEmail.body as Email;
+
+        const responseFindEmail = await request(app).get("/email/find").send({
+            e_mail,
+        });
+
+        expect(responseFindEmail.status).toBe(200);
+    });
+});
