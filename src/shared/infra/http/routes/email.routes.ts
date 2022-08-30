c7d13c55607418cae064e3f0ed7ce4c4933dc83d
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-29 21:59:54 -0300

    create rota get p/ retornar email buscado

--- src/shared/infra/http/routes/email.routes.ts
+++ src/shared/infra/http/routes/email.routes.ts
@@ -1,4 +1,5 @@
 import { CreateEmailController } from "@modules/author/useCases/createEmail/CreateEmailController";
+import { FindEmailController } from "@modules/author/useCases/findEmail/FindEmailController";
 import { ListEmailsController } from "@modules/author/useCases/listEmails/ListEmailsController";
 import { Router } from "express";
 
@@ -8,6 +9,10 @@ const createEmailController = new CreateEmailController();
 
 const listEmailsController = new ListEmailsController();
 
+const findEmailController = new FindEmailController();
+
 emailRoutes.post("/", createEmailController.handle);
 
 emailRoutes.get("/", listEmailsController.handle);
+
+emailRoutes.get("/find", findEmailController.handle);
