544cbcda3aabfc52d46221ca50517c7cb701fb4a
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-30 18:13:47 -0300

    create repo em memoria de ingredients c/ metodos

---
+++ src/modules/recipe/repositories/in-Memory/IngredientRepositoryInMemory.ts
@@ -0,0 +1,39 @@
+import { ICreateIngredientDTO } from "@modules/recipe/dtos/ICreateIngredientDTO";
+import { Ingredient } from "@modules/recipe/infra/typeorm/entities/Ingredient";
+
+import { IIngredientRepository } from "../IIngredientRepository";
+
+export class IngredientRepositoryInMemory implements IIngredientRepository {
+    create(data: ICreateIngredientDTO): Promise<Ingredient> {
+        throw new Error("Method not implemented.");
+    }
+    list(): Promise<Ingredient[]> {
+        throw new Error("Method not implemented.");
+    }
+    findIngredientByProdutoName(produto_name: string): Promise<Ingredient> {
+        throw new Error("Method not implemented.");
+    }
+    findAllIngredientByProdutoName(
+        produto_name: string
+    ): Promise<Ingredient[]> {
+        throw new Error("Method not implemented.");
+    }
+    findIngredientByProdutoNameWeightUnity(
+        produto_name: string,
+        weight: number,
+        unity: number
+    ): Promise<Ingredient> {
+        throw new Error("Method not implemented.");
+    }
+    updateIngredientById(
+        id: string,
+        produto_name?: string,
+        weight?: number,
+        unity?: number
+    ): Promise<Ingredient> {
+        throw new Error("Method not implemented.");
+    }
+    deleteIngredientById(id: string): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+}
