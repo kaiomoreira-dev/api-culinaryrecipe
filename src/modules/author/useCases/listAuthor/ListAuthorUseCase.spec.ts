a187ac2131b7f56432c459e36543285a9814d142
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-30 23:26:27 -0300

    create ingrediente em receita de authors

--- src/modules/author/useCases/listAuthor/ListAuthorUseCase.spec.ts
+++ src/modules/author/useCases/listAuthor/ListAuthorUseCase.spec.ts
@@ -70,6 +70,17 @@ describe("List authors UseCase", () => {
                     time: 5,
                     total_guests: 5,
                     additional_features: "low custom",
+                    ingredients: [
+                        {
+                            id: faker.datatype.uuid(),
+                            description: faker.lorem.paragraphs(),
+                            produto_name: "Alho roxo",
+                            unity: 1,
+                            weight: 100,
+                            created_at: String(faker.datatype.datetime()),
+                            updated_at: String(faker.datatype.datetime()),
+                        },
+                    ],
                     created_at: faker.datatype.datetime(),
                     updated_at: faker.datatype.datetime(),
                 },
