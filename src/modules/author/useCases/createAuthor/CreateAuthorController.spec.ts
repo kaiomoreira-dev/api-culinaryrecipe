809d4969e4a95b8c645cd69364c46b861f2ebbf1
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-30 23:30:14 -0300

    remove import de author

--- src/modules/author/useCases/createAuthor/CreateAuthorController.spec.ts
+++ src/modules/author/useCases/createAuthor/CreateAuthorController.spec.ts
@@ -1,6 +1,5 @@
 import "dotenv/config";
 import { faker } from "@faker-js/faker";
-import { Author } from "@modules/author/infra/typeorm/entities/Author";
 import request from "supertest";
 import { DataSource } from "typeorm";
 
