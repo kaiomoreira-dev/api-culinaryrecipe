dcab9f1086a35d8cd519c59e2c5eedd42d7bf064
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-30 22:47:51 -0300

    importing cases e repository p/ testar list author

---
+++ src/modules/author/useCases/listAuthor/ListAuthorUseCase.spec.ts
@@ -0,0 +1,20 @@
+import { AuthorRepositoryInMemory } from "@modules/author/repositories/in-Memory/AuthorRepositoryInMemory";
+import { EmailRepositoryInMemory } from "@modules/author/repositories/in-Memory/EmailRepositoryInMemory";
+import { IngredientRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/IngredientRepositoryInMemory";
+import { ProdutoRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/ProdutoRepositoryInMemory";
+import { RecipeRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/RecipeRepositoryInMemory";
+import { CreateIngredientUseCase } from "@modules/recipe/useCases/createIngredient/CreateIngredientUseCase";
+import { CreateRecipeUseCase } from "@modules/recipe/useCases/createRecipe/CreateRecipeUseCase";
+
+import { CreateAuthorUseCase } from "../createAuthor/CreateAuthorUseCase";
+import { CreateEmailUseCase } from "../createEmail/CreateEmailUseCase";
+
+let produtoRepositoryInMemory: ProdutoRepositoryInMemory;
+let ingredientRepositoryInMemory: IngredientRepositoryInMemory;
+let emailRepositoryInMemory: EmailRepositoryInMemory;
+let recipeRepositoryInMemory: RecipeRepositoryInMemory;
+let authorRepositoryInMemory: AuthorRepositoryInMemory;
+let createAuthorUseCase: CreateAuthorUseCase;
+let createEmailUseCase: CreateEmailUseCase;
+let createRecipeUseCase: CreateRecipeUseCase;
+let createIngredientUseCase: CreateIngredientUseCase;
