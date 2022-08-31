d6036de46c490f0aea113a0c522fa06e2ebd8605
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-30 22:10:48 -0300

    remove metodo findAuthorByEmail

--- src/modules/author/repositories/IAuthorRepository.ts
+++ src/modules/author/repositories/IAuthorRepository.ts
@@ -4,7 +4,6 @@ import { Author } from "../infra/typeorm/entities/Author";
 export interface IAuthorRepository {
     create(data: ICreateAuthorDTO): Promise<Author>;
     list(): Promise<Author[]>;
-    findAuthorByEmail(e_mail: string): Promise<Author>;
     findAuthorByName(name: string): Promise<Author>;
     deleteAuthorById(id: string): Promise<void>;
 }
