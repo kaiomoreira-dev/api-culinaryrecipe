4c49dec44e0156ce8b6cc9eb94ec11c7834443f1
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-30 23:31:56 -0300

    create beforEach p/ criar migrations e connction

--- src/modules/author/useCases/listAuthor/ListAuthorController.spec.ts
+++ src/modules/author/useCases/listAuthor/ListAuthorController.spec.ts
@@ -8,4 +8,10 @@ import { createConnection } from "@shared/infra/typeorm";
 
 let connection: DataSource;
 
-describe("List author Controller", () => {});
+describe("List author Controller", () => {
+    beforeAll(async () => {
+        connection = await createConnection("localhost");
+
+        await connection.runMigrations();
+    });
+});
