37ae5e65fddcdf5b6499a61a23635d8a0e18e0ee
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-08-30 23:32:25 -0300

    create afterEach p/ dropar e fechar base dedados

--- src/modules/author/useCases/listAuthor/ListAuthorController.spec.ts
+++ src/modules/author/useCases/listAuthor/ListAuthorController.spec.ts
@@ -14,4 +14,10 @@ describe("List author Controller", () => {
 
         await connection.runMigrations();
     });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+
+        await connection.destroy();
+    });
 });
