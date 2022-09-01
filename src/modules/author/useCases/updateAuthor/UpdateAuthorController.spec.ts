66dbd38e45e27bf8ecf07016a6d83c07302553b1
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-01 16:05:27 -0300

    create test controller p/ atualizar author p/ name

--- src/modules/author/useCases/updateAuthor/UpdateAuthorController.spec.ts
+++ src/modules/author/useCases/updateAuthor/UpdateAuthorController.spec.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import { faker } from "@faker-js/faker";
+import { Author } from "@modules/author/infra/typeorm/entities/Author";
 import request from "supertest";
 import { DataSource } from "typeorm";
 
@@ -19,5 +20,28 @@ describe("Update author Controller", () => {
         await connection.dropDatabase();
 
         await connection.destroy();
+    });
+
+    it("should be able to update author using name", async () => {
+        const responseAuthor = await request(app).post("/author").send({
+            id: faker.datatype.uuid(),
+            name: "Kaio Moreira",
+            whatsapp: faker.phone.number(),
+        });
+
+        const { name } = responseAuthor.body as Author;
+
+        const newName = "Kaio dos Santos Moreira";
+        const whatsapp = "1234567890";
+
+        const responseAuthorUpdated = await request(app)
+            .put("/author/update")
+            .send({
+                name,
+                newName,
+                whatsapp,
+            });
+
+        expect(responseAuthorUpdated.status).toBe(200);
     });
 });
