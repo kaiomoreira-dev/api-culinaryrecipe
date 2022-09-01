5847cd93dc66958972b297a6bd1b0a56defafb7f
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-01 17:50:12 -0300

    create afterEach p/ dropar connection e migrations

--- src/modules/author/useCases/deleteAuthor/DeleteAuthorController.spec.ts
+++ src/modules/author/useCases/deleteAuthor/DeleteAuthorController.spec.ts
@@ -14,4 +14,10 @@ describe("Delete author Controller", () => {
 
         await connection.runMigrations();
     });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+
+        await connection.destroy();
+    });
 });
