02302779f3a809debb3b0f8d98c8b9205c9f7010
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-01 20:31:05 -0300

    import repositories e case p/ criar recipes

---
+++ src/modules/recipe/useCases/createRecipe/CreateRecipeUseCase.spec.ts
@@ -0,0 +1,18 @@
+import { AuthorRepositoryInMemory } from "@modules/author/repositories/in-Memory/AuthorRepositoryInMemory";
+import { CreateAuthorUseCase } from "@modules/author/useCases/createAuthor/CreateAuthorUseCase";
+import { IngredientRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/IngredientRepositoryInMemory";
+import { ProdutoRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/ProdutoRepositoryInMemory";
+import { RecipeRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/RecipeRepositoryInMemory";
+
+import { CreateIngredientUseCase } from "../createIngredient/CreateIngredientUseCase";
+import { CreateProdutoUseCase } from "../createProduto/CreateProdutoUseCase";
+import { CreateRecipeUseCase } from "./CreateRecipeUseCase";
+
+let produtoRepositoryInMemory: ProdutoRepositoryInMemory;
+let ingredientRepositoryInMemory: IngredientRepositoryInMemory;
+let authorRepositoryInMemory: AuthorRepositoryInMemory;
+let recipeRepositoryInMemory: RecipeRepositoryInMemory;
+let createRecipeUseCase: CreateRecipeUseCase;
+let createAuthorUseCase: CreateAuthorUseCase;
+let createIngredientUseCase: CreateIngredientUseCase;
+let createProdutoUseCase: CreateProdutoUseCase;
