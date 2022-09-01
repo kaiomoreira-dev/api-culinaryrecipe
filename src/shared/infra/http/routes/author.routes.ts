c4976f695ae11e8c898046626e712bf12c55565a
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-01 14:55:30 -0300

    create rota put p/ update name, whatsapp em author

--- src/shared/infra/http/routes/author.routes.ts
+++ src/shared/infra/http/routes/author.routes.ts
@@ -1,6 +1,7 @@
 import { CreateAuthorController } from "@modules/author/useCases/createAuthor/CreateAuthorController";
 import { FindAuthorByNameController } from "@modules/author/useCases/findAuthor/FindAuthorByNameController";
 import { ListAuthorController } from "@modules/author/useCases/listAuthor/ListAuthorController";
+import { UpdateAuthorController } from "@modules/author/useCases/updateAuthor/UpdateAuthorUseController";
 import { Router } from "express";
 
 export const authorRoutes = Router();
@@ -11,8 +12,12 @@ const listAuthorController = new ListAuthorController();
 
 const findAuthorByNameController = new FindAuthorByNameController();
 
+const updateAuthorController = new UpdateAuthorController();
+
 authorRoutes.post("/", createAuthorController.handle);
 
 authorRoutes.get("/", listAuthorController.handle);
 
 authorRoutes.get("/find", findAuthorByNameController.handle);
+
+authorRoutes.put("/update", updateAuthorController.handle);
