4418d175dbed94c9f71f3406daf0ed3c8dbed65f
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-01 10:27:35 -0300

    create validator de author p/ name

--- src/modules/author/useCases/findAuthor/FindAuthorByNameUseCase.ts
+++ src/modules/author/useCases/findAuthor/FindAuthorByNameUseCase.ts
@@ -2,6 +2,8 @@ import { Author } from "@modules/author/infra/typeorm/entities/Author";
 import { IAuthorRepository } from "@modules/author/repositories/IAuthorRepository";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "@shared/errors/AppError";
+
 @injectable()
 export class FindAuthorByNameUseCase {
     constructor(
@@ -9,5 +11,13 @@ export class FindAuthorByNameUseCase {
         private authorRepository: IAuthorRepository
     ) {}
 
-    async execute(author_name: string): Promise<Author> {}
+    async execute(author_name: string): Promise<Author> {
+        const authorValidator = await this.authorRepository.findAuthorByName(
+            author_name
+        );
+
+        if (!authorValidator) {
+            throw new AppError("Author not found", 404);
+        }
+    }
 }
