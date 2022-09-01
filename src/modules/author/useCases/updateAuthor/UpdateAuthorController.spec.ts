e6501383a4bffcd785a00d861058532091f3e747
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-01 16:00:17 -0300

    create beforEach p/ criar migration e connection

--- src/modules/author/useCases/updateAuthor/UpdateAuthorController.spec.ts
+++ src/modules/author/useCases/updateAuthor/UpdateAuthorController.spec.ts
@@ -8,4 +8,10 @@ import { createConnection } from "@shared/infra/typeorm";
 
 let connection: DataSource;
 
-describe("Update author Controller", () => {});
+describe("Update author Controller", () => {
+    beforeAll(async () => {
+        connection = await createConnection("localhost");
+
+        await connection.runMigrations();
+    });
+});
