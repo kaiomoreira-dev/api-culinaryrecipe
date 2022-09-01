6e3ac4ea5c8854633985cabdc62e747848a77872
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-01 17:07:26 -0300

    create validação em author p/ saber se existe

--- src/modules/author/useCases/deleteAuthor/DeleteAuthorUseCase.ts
+++ src/modules/author/useCases/deleteAuthor/DeleteAuthorUseCase.ts
@@ -1,6 +1,8 @@
 import { IAuthorRepository } from "@modules/author/repositories/IAuthorRepository";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "@shared/errors/AppError";
+
 @injectable()
 export class DeleteAuthorUseCase {
     constructor(
@@ -8,5 +10,13 @@ export class DeleteAuthorUseCase {
         private authorRepository: IAuthorRepository
     ) {}
 
-    async execute(name: string): Promise<void> {}
+    async execute(name: string): Promise<void> {
+        const authorValidator = await this.authorRepository.findAuthorByName(
+            name
+        );
+
+        if (!authorValidator) {
+            throw new AppError("Author not found", 404);
+        }
+    }
 }
