750dbae43ec34509262cd455d0739c3287f6319f
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-01 10:26:26 -0300

    create execute p/ encontrar author p/ name

--- src/modules/author/useCases/findAuthor/FindAuthorByNameUseCase.ts
+++ src/modules/author/useCases/findAuthor/FindAuthorByNameUseCase.ts
@@ -1,3 +1,4 @@
+import { Author } from "@modules/author/infra/typeorm/entities/Author";
 import { IAuthorRepository } from "@modules/author/repositories/IAuthorRepository";
 import { inject, injectable } from "tsyringe";
 
@@ -7,4 +8,6 @@ export class FindAuthorByNameUseCase {
         @inject("AuthorRepository")
         private authorRepository: IAuthorRepository
     ) {}
+
+    async execute(author_name: string): Promise<Author> {}
 }
