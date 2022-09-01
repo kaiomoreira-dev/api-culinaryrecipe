3dff2493991b3e739fdbb1e567ed0eb7529409a6
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-01 14:37:34 -0300

    create metodo p/ buscar author p/ whatsapp

--- src/modules/author/repositories/IAuthorRepository.ts
+++ src/modules/author/repositories/IAuthorRepository.ts
@@ -4,11 +4,15 @@ import { Author } from "../infra/typeorm/entities/Author";
 export interface IAuthorRepository {
     create(data: ICreateAuthorDTO): Promise<Author>;
     list(): Promise<Author[]>;
+
     findAuthorByName(name: string): Promise<Author>;
+    findAuthorByWhatsapp(whatsapp: string): Promise<Author>;
+
     updateNameAndWhatsappByName(
         name: string,
         new_name: string,
         whatsapp: string
     ): Promise<Author>;
+
     deleteAuthorByName(name: string): Promise<void>;
 }
