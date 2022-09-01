67edeb29faa53da989007362f7cab92d0ebbf53e
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-01 14:49:40 -0300

    implement handle p/ atualizar author e retornar-lo

--- src/modules/author/useCases/updateAuthor/UpdateAuthorUseController.ts
+++ src/modules/author/useCases/updateAuthor/UpdateAuthorUseController.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { UpdateAuthorUseCase } from "./UpdateAuthorUseCase";
 
 export class UpdateAuthorController {
-    async handle(request: Request, response: Response): Promise<Response> {}
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { name, newName, whatsapp } = request.body;
+
+        const updateAuthorUseCase = container.resolve(UpdateAuthorUseCase);
+
+        const updateAuthor = await updateAuthorUseCase.execute(
+            name,
+            newName,
+            whatsapp
+        );
+
+        return response.status(200).json(updateAuthor);
+    }
 }
