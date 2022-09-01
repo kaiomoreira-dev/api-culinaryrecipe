9615430c2eb356a1df3ff6c861fcd5090f0bbe6c
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-01 17:13:45 -0300

    implement handle p/ deletar author p/ name

--- src/modules/author/useCases/deleteAuthor/DeleteAuthorController.ts
+++ src/modules/author/useCases/deleteAuthor/DeleteAuthorController.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { DeleteAuthorUseCase } from "./DeleteAuthorUseCase";
 
 export class DeleteAuthorController {
-    async handle(request: Request, response: Response): Promise<Response> {}
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { name } = request.body;
+
+        const deleteAuthorUseCase = container.resolve(DeleteAuthorUseCase);
+
+        await deleteAuthorUseCase.execute(name);
+
+        return response
+            .status(200)
+            .json({ message: "Author deleted successfully!" });
+    }
 }
