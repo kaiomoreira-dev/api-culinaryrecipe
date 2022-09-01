05d0fa7e2d9802fd141a93ef0c1be31be1b72162
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-01 10:31:42 -0300

    implement metodo handle p/ retornar author p/ name

--- src/modules/author/useCases/findAuthor/FindAuthorByNameController.ts
+++ src/modules/author/useCases/findAuthor/FindAuthorByNameController.ts
@@ -1,5 +1,16 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { FindAuthorByNameUseCase } from "./FindAuthorByNameUseCase";
 
 export class FindAuthorByNameController {
-    async handle(request: Request, response: Response): Promise<Response> {}
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { author_name } = request.body;
+
+        const findAuthorByName = container.resolve(FindAuthorByNameUseCase);
+
+        const author = await findAuthorByName.execute(author_name);
+
+        return response.status(200).json(author);
+    }
 }
