f099d124bd3ae3997190d5659357f1475a7e191d
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-01 15:58:47 -0300

    import configurações p/ test de controller

---
+++ src/modules/author/useCases/updateAuthor/UpdateAuthorController.spec.ts
@@ -0,0 +1,7 @@
+import "dotenv/config";
+import { faker } from "@faker-js/faker";
+import request from "supertest";
+import { DataSource } from "typeorm";
+
+import { app } from "@shared/infra/http/app";
+import { createConnection } from "@shared/infra/typeorm";
