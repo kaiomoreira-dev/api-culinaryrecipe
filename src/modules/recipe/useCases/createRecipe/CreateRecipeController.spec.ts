001232479a43a8f35e6bb2f5788445051acfcebb
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-01 22:20:43 -0300

    create afterEach p/ dropa base e connection

--- src/modules/recipe/useCases/createRecipe/CreateRecipeController.spec.ts
+++ src/modules/recipe/useCases/createRecipe/CreateRecipeController.spec.ts
@@ -14,4 +14,10 @@ describe("Create recipe Controller", () => {
 
         await connection.runMigrations();
     });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+
+        await connection.destroy();
+    });
 });
