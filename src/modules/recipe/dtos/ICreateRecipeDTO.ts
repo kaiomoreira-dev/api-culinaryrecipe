b1f7b86a13ef113338889dc592a179e4bc47dc7c
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-01 21:13:19 -0300

    create test p/ validar a criação de recipe

--- src/modules/recipe/dtos/ICreateRecipeDTO.ts
+++ src/modules/recipe/dtos/ICreateRecipeDTO.ts
@@ -1,26 +1,14 @@
 import { Ingredient } from "../infra/typeorm/entities/Ingredient";
 
-enum Difficulty {
-    easy = "easy",
-    medium = "medium",
-    hard = "hard",
-}
-
-enum DishType {
-    appetizer = "appetizer",
-    main_course = "main course",
-    dessert = "dessert",
-}
-
 export interface ICreateRecipeDTO {
     id?: string;
     name: string;
     description: string;
-    ingredients: Ingredient[];
+    ingredients?: Ingredient[];
     author_name: string;
     time: number;
-    difficulty: Difficulty;
-    dish_type: DishType;
+    difficulty: "easy" | "medium" | "hard";
+    dish_type: "appetizer" | "main_course" | "dessert";
     additional_features: string;
     total_guests: number;
 }
