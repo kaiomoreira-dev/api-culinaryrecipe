5c6f291211a24756883c9614f78ec01b7b8df52e
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-03 16:40:49 -0300

    alter parametro p/ id ao deletar, update etc..

--- src/modules/recipe/repositories/IRecipeRepository.ts
+++ src/modules/recipe/repositories/IRecipeRepository.ts
@@ -4,20 +4,13 @@ import { Recipe } from "../infra/typeorm/entities/Recipe";
 export interface IRecipeRepository {
     create(data: ICreateRecipeDTO): Promise<Recipe>;
     list(): Promise<Recipe[]>;
-    listAllRecipeByIngredientProdutoName(
-        produto_name: string
-    ): Promise<Recipe[]>;
+    listAllRecipeByIngredientProdutoId(produto_id: string): Promise<Recipe[]>;
 
-    findRecipeById(id: string): Promise<Recipe>;
     findRecipesByDifficulty(difficulty: string): Promise<Recipe[]>;
-    findRecipeByAuthor(author_name: string): Promise<Recipe>;
+    findRecipeByAuthor(author_id: string): Promise<Recipe>;
     findRecipeByName(name: string): Promise<Recipe>;
 
     updateTimeByRecipeId(id: string, time: number): Promise<Recipe>;
-    updateAuthorNameByRecipeId(
-        id: string,
-        author_name: string
-    ): Promise<Recipe>;
 
     deleteRecipeById(id: string): Promise<void>;
 }
