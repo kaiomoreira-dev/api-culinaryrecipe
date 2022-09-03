f0100c612d96d357bf30dbde6f914df643747b3b
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-03 14:37:06 -0300

    alter author_name p/ author_id controler d email

--- src/modules/author/useCases/createEmail/CreateEmailController.ts
+++ src/modules/author/useCases/createEmail/CreateEmailController.ts
@@ -5,11 +5,11 @@ import { CreateEmailUseCase } from "./CreateEmailUseCase";
 
 export class CreateEmailController {
     async handle(request: Request, response: Response): Promise<Response> {
-        const { e_mail, author_name } = request.body;
+        const { e_mail, author_id } = request.body;
 
         const createEmailUseCase = container.resolve(CreateEmailUseCase);
 
-        const email = await createEmailUseCase.execute({ e_mail, author_name });
+        const email = await createEmailUseCase.execute({ e_mail, author_id });
 
         return response.status(200).json(email);
     }
