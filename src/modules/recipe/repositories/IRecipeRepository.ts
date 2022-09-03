aeaf2e95dc854e30b12f92c7eb7886ff164f11c0
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-03 19:32:13 -0300

    alter metodo p/ findById na interface recipe

--- src/modules/recipe/repositories/IRecipeRepository.ts
+++ src/modules/recipe/repositories/IRecipeRepository.ts
@@ -5,7 +5,7 @@ export interface IRecipeRepository {
     create(data: ICreateRecipeDTO): Promise<Recipe>;
     list(): Promise<Recipe[]>;
 
-    findRecipeById(id: string): Promise<Recipe>;
+    findById(id: string): Promise<Recipe>;
     findRecipesByDifficulty(difficulty: string): Promise<Recipe[]>;
     findRecipeByAuthor(author_id: string): Promise<Recipe>;
     findRecipeByName(name: string): Promise<Recipe>;
