7ab0d31561e269624d4e4e4c71e27550df982ccd
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-03 22:06:24 -0300

    alter metodo p/ findByAuthorId repo recipe

--- src/modules/recipe/infra/typeorm/repositories/RecipeRepository.ts
+++ src/modules/recipe/infra/typeorm/repositories/RecipeRepository.ts
@@ -34,7 +34,7 @@ export class RecipeRepository implements IRecipeRepository {
     async findByName(name: string): Promise<Recipe> {
         return this.repository.findOneBy({ name });
     }
-    async findRecipeByAuthor(author_id: string): Promise<Recipe> {
+    async findByAuthorId(author_id: string): Promise<Recipe> {
         return this.repository.findOneBy({ author_id });
     }
 
