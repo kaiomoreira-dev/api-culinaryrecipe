0bd3dd8cee8f8757587aed2f3252eb4588ac43c9
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-05 11:17:08 -0300

    alter controler recipe-ing. p/ receber ing.id

--- src/modules/recipe/useCases/recipesIngredients/listByIngredient/ListRecipesByIngredientController.ts
+++ src/modules/recipe/useCases/recipesIngredients/listByIngredient/ListRecipesByIngredientController.ts
@@ -1,19 +1,19 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 
-import { ListAllRecipesByImgredientProdutoNameUseCase } from "./ListRecipesByIngredientUseCase";
+import { ListRecipesByIngredientUseCase } from "./ListRecipesByIngredientUseCase";
 
 export class ListRecipesByIngredientController {
     async handle(request: Request, response: Response): Promise<Response> {
-        const { produto_name } = request.body;
+        const { ingredient_id } = request.body;
 
         const listAllRecipesByImgredientProdutoNameUseCase = container.resolve(
-            ListAllRecipesByImgredientProdutoNameUseCase
+            ListRecipesByIngredientUseCase
         );
 
         const recipes =
             await listAllRecipesByImgredientProdutoNameUseCase.execute(
-                produto_name
+                ingredient_id
             );
 
         return response.status(200).json(recipes);
