10bcf6dcbd7e5eb4c1f036ae3dbc3c8e36b39d2b
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-05 11:02:45 -0300

    alter recipe route p/ listRecipesByIngredient

--- src/shared/infra/http/routes/recipe.routes.ts
+++ src/shared/infra/http/routes/recipe.routes.ts
@@ -1,18 +1,18 @@
 import { CreateRecipeController } from "@modules/recipe/useCases/createRecipe/CreateRecipeController";
-import { ListAllRecipesByImgredientProdutoNameController } from "@modules/recipe/useCases/recipesIngredients/listAllRecipesByIngredientProdutoName/ListAllRecipesByImgredientProdutoNameController";
+import { ListRecipesByIngredientController } from "@modules/recipe/useCases/recipesIngredients/listByIngredient/ListRecipesByIngredientController";
 import { Router } from "express";
 
 export const recipeRoutes = Router();
 
 const createRecipeController = new CreateRecipeController();
 
-const listAllRecipesByImgredientProdutoNameController =
-  new ListAllRecipesByImgredientProdutoNameController();
+const listRecipesByIngredientController =
+  new ListRecipesByIngredientController();
 
 recipeRoutes.post("/", createRecipeController.handle);
 
 //= =============Recipes-Ingredients========================
 recipeRoutes.get(
   "/ingredient/produtoname",
-  listAllRecipesByImgredientProdutoNameController.handle
+  listRecipesByIngredientController.handle
 );
