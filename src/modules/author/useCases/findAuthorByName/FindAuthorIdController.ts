ef81073812abb33f913d40716f4bba334bc5efc3
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-05 17:45:49 -0300

    alter name controller p/ findAuthorIdC

---
+++ src/modules/author/useCases/findAuthorByName/FindAuthorIdController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { FindAuthorIdUseCase } from "./FindAuthorIdUseCase";
+
+export class FindAuthorIdController {
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { author_id } = request.body;
+
+        const findAuthorByName = container.resolve(FindAuthorIdUseCase);
+
+        const author = await findAuthorByName.execute(author_id);
+
+        return response.status(200).json(author);
+    }
+}
