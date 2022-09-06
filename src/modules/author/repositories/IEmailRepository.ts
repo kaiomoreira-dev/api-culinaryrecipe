ac65db48db11ce8a09a6927c1ca88bb69c4aa1ef
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-06 09:40:49 -0300

    create metodo findById interface email

--- src/modules/author/repositories/IEmailRepository.ts
+++ src/modules/author/repositories/IEmailRepository.ts
@@ -6,6 +6,7 @@ export interface IEmailRepository {
     create(data: ICreateEmailDTO): Promise<Email>;
     list(): Promise<Email[]>;
 
+    findById(id: string): Promise<Email>;
     findByEmail(e_mail: string): Promise<Email>;
 
     updateById(id: string, newE_mail: string): Promise<Email>;
