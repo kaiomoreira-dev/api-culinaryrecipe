585778c8ce173287609af0b4b82f25fcc8803f16
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-09 11:05:49 -0300

    remove instance,validações d author em recipe case

--- src/modules/recipe/useCases/createRecipe/CreateRecipeUseCase.ts
+++ src/modules/recipe/useCases/createRecipe/CreateRecipeUseCase.ts
@@ -1,7 +1,6 @@
 /* eslint-disable prefer-const */
 /* eslint-disable no-await-in-loop */
 /* eslint-disable no-restricted-syntax */
-import { IAuthorRepository } from "@modules/author/repositories/IAuthorRepository";
 import { ICreateRecipeDTO } from "@modules/recipe/dtos/ICreateRecipeDTO";
 import { Ingredient } from "@modules/recipe/infra/typeorm/entities/Ingredient";
 import { Recipe } from "@modules/recipe/infra/typeorm/entities/Recipe";
@@ -18,10 +17,7 @@ export class CreateRecipeUseCase {
         private recipeRepository: IRecipeRepository,
 
         @inject("IngredientRepository")
-        private ingredientRepository: IIngredientRepository,
-
-        @inject("AuthorRepository")
-        private authorRepository: IAuthorRepository
+        private ingredientRepository: IIngredientRepository
     ) {}
 
     async execute(
@@ -34,7 +30,6 @@ export class CreateRecipeUseCase {
             additional_features,
             time,
             total_guests,
-            author_id,
         }: ICreateRecipeDTO,
         ingredients: string[]
     ): Promise<Recipe> {
@@ -44,14 +39,6 @@ export class CreateRecipeUseCase {
 
         if (dish_type !== "appetizer" && "main course" && "dessert") {
             throw new AppError("dish_type incorrect!", 401);
-        }
-
-        // buscando name de autor
-        const authorValidator = await this.authorRepository.findById(author_id);
-
-        // validando se author existe
-        if (!authorValidator) {
-            throw new AppError("Author not found", 404);
         }
 
         // array vazio de Ingredient
@@ -89,7 +76,6 @@ export class CreateRecipeUseCase {
             time,
             total_guests,
             ingredients: addIngredients,
-            author_id,
         });
 
         return recipe;
