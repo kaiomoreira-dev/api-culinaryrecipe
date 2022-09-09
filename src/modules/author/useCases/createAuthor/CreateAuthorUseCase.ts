71a06fb2dcd4d303ff1a830c1679169cc52656bf
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-09 10:53:49 -0300

    create updateAuthorIdById em create author case

--- src/modules/author/useCases/createAuthor/CreateAuthorUseCase.ts
+++ src/modules/author/useCases/createAuthor/CreateAuthorUseCase.ts
@@ -2,20 +2,41 @@
 /* eslint-disable no-await-in-loop */
 /* eslint-disable prefer-const */
 import { ICreateAuthorDTO } from "@modules/author/dtos/ICreateAuthorDTO";
-import { Author } from "@modules/author/infra/typeorm/entities/Author";
+import { Email } from "@modules/author/infra/typeorm/entities/Email";
 import { IAuthorRepository } from "@modules/author/repositories/IAuthorRepository";
+import { IEmailRepository } from "@modules/author/repositories/IEmailRepository";
+import { Recipe } from "@modules/recipe/infra/typeorm/entities/Recipe";
+import { IRecipeRepository } from "@modules/recipe/repositories/IRecipeRepository";
 import { inject, injectable } from "tsyringe";
 
 import { AppError } from "@shared/errors/AppError";
+
+interface IAuthorInfo {
+    id: string;
+    name: string;
+    whatsapp: string;
+    emails: Email[];
+    recipes: Recipe[];
+}
 
 @injectable()
 export class CreateAuthorUseCase {
     constructor(
         @inject("AuthorRepository")
-        private authorRepository: IAuthorRepository
+        private authorRepository: IAuthorRepository,
+
+        @inject("EmailRepository")
+        private emailRepository: IEmailRepository,
+
+        @inject("RecipeRepository")
+        private recipeRepository: IRecipeRepository
     ) {}
 
-    async execute({ id, name, whatsapp }: ICreateAuthorDTO): Promise<Author> {
+    async execute(
+        { id, name, whatsapp }: ICreateAuthorDTO,
+        emails: string[],
+        recipes?: string[]
+    ): Promise<IAuthorInfo> {
         // buscando name de autor
         const authorValidator = await this.authorRepository.findByName(name);
 
@@ -39,6 +60,57 @@ export class CreateAuthorUseCase {
             whatsapp,
         });
 
-        return author;
+        // criar array de emails atualizado
+        let arrEmails: Email[] = [];
+
+        // verificar se existe cada email
+        for (const id of emails) {
+            const emailValidator = await this.emailRepository.findById(id);
+            if (!emailValidator) {
+                await this.authorRepository.deleteById(author.id);
+                throw new AppError("Email is not exists.", 404);
+            }
+
+            const emailUpdated = await this.emailRepository.updateAuthorIdById(
+                id,
+                author.id
+            );
+
+            arrEmails.push(emailUpdated);
+        }
+
+        // cria array recipe atualizado
+        let arrRecipe: Recipe[] = [];
+
+        // verificar se existe cada recipe
+        for (const id of recipes) {
+            const recipeValidator = await this.recipeRepository.findById(id);
+            if (!recipeValidator) {
+                await this.authorRepository.deleteById(author.id);
+                throw new AppError("Recipe is not exists.", 404);
+            }
+
+            const recipeUpdated = await this.recipeRepository.updateAuthorId(
+                id,
+                author.id
+            );
+
+            console.log(recipeUpdated);
+
+            arrRecipe.push(recipeUpdated);
+        }
+        // atualizar author_id em email
+
+        // atualizar author_id em recipe
+
+        const authorInfo: IAuthorInfo = {
+            id: author.id,
+            name: author.name,
+            whatsapp: author.whatsapp,
+            emails: arrEmails,
+            recipes: arrRecipe,
+        };
+
+        return authorInfo;
     }
 }
