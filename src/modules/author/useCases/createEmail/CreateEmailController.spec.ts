b0a37209dae2ff51aa7cc28f4ccd6e721118925f
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-10 15:06:10 -0300

    remove author ao criar email no controller

--- src/modules/author/useCases/createEmail/CreateEmailController.spec.ts
+++ src/modules/author/useCases/createEmail/CreateEmailController.spec.ts
@@ -1,6 +1,5 @@
 import "dotenv/config";
 import { faker } from "@faker-js/faker";
-import { Author } from "@modules/author/infra/typeorm/entities/Author";
 import request from "supertest";
 import { DataSource } from "typeorm";
 
@@ -23,54 +22,25 @@ describe("Create e-mail Controller", () => {
     });
 
     it("should be able to create a new email", async () => {
-        const responseAuthor = await request(app).post("/author").send({
-            id: faker.datatype.uuid(),
-            name: faker.name.fullName(),
-            whatsapp: faker.phone.number(),
-        });
-
-        const author = responseAuthor.body as Author;
-
         const response = await request(app).post("/email").send({
             id: faker.datatype.uuid(),
             e_mail: faker.internet.email(),
-            author_id: author.id,
         });
 
         expect(response.status).toEqual(200);
     });
 
     it("should not be able to create a new email with same", async () => {
-        const responseAuthor = await request(app).post("/author").send({
-            id: faker.datatype.uuid(),
-            name: faker.name.fullName(),
-            whatsapp: faker.phone.number(),
-        });
-
-        const author = responseAuthor.body as Author;
-
         await request(app).post("/email").send({
             id: faker.datatype.uuid(),
             e_mail: "test@test.com",
-            author_id: author.id,
         });
 
         const response = await request(app).post("/email").send({
             id: faker.datatype.uuid(),
             e_mail: "test@test.com",
-            author_id: author.id,
         });
 
         expect(response.status).toBe(401);
-    });
-
-    it("should not be able to create a new email with author_id not found", async () => {
-        const response = await request(app).post("/email").send({
-            id: faker.datatype.uuid(),
-            e_mail: "test@test.com",
-            author_id: faker.datatype.uuid(),
-        });
-
-        expect(response.status).toBe(404);
     });
 });
