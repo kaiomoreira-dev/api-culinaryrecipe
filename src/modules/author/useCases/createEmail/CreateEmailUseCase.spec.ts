310f383a5b154425f64ffff9a41a380cba833d31
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-10 15:06:45 -0300

    remove instancias de author em create email test

--- src/modules/author/useCases/createEmail/CreateEmailUseCase.spec.ts
+++ src/modules/author/useCases/createEmail/CreateEmailUseCase.spec.ts
@@ -1,19 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { faker } from "@faker-js/faker";
-import { ICreateAuthorDTO } from "@modules/author/dtos/ICreateAuthorDTO";
 import { ICreateEmailDTO } from "@modules/author/dtos/ICreateEmailDTO";
 import { AuthorRepositoryInMemory } from "@modules/author/repositories/in-Memory/AuthorRepositoryInMemory";
 import { EmailRepositoryInMemory } from "@modules/author/repositories/in-Memory/EmailRepositoryInMemory";
-import { RecipeRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/RecipeRepositoryInMemory";
 
 import { AppError } from "@shared/errors/AppError";
 
-import { CreateAuthorUseCase } from "../createAuthor/CreateAuthorUseCase";
 import { CreateEmailUseCase } from "./CreateEmailUseCase";
 
 let emailRepositoryInMemory: EmailRepositoryInMemory;
 let authorRepositoryInMemory: AuthorRepositoryInMemory;
-let createAuthorUseCase: CreateAuthorUseCase;
 let createEmailUseCase: CreateEmailUseCase;
 
 describe("Create e-mail UseCase", () => {
@@ -21,26 +17,13 @@ describe("Create e-mail UseCase", () => {
         emailRepositoryInMemory = new EmailRepositoryInMemory();
         authorRepositoryInMemory = new AuthorRepositoryInMemory();
 
-        createAuthorUseCase = new CreateAuthorUseCase(authorRepositoryInMemory);
-        createEmailUseCase = new CreateEmailUseCase(
-            emailRepositoryInMemory,
-            authorRepositoryInMemory
-        );
+        createEmailUseCase = new CreateEmailUseCase(emailRepositoryInMemory);
     });
 
     it("should be able to create a new e-mail", async () => {
-        const author: ICreateAuthorDTO = {
-            id: faker.datatype.uuid(),
-            name: faker.name.fullName(),
-            whatsapp: faker.phone.number(),
-        };
-
-        const authorCreated = await createAuthorUseCase.execute(author);
-
         const email: ICreateEmailDTO = {
             id: faker.datatype.uuid(),
             e_mail: faker.internet.email(),
-            author_id: authorCreated.id,
         };
 
         const e_mail = await createEmailUseCase.execute(email);
@@ -49,42 +32,20 @@ describe("Create e-mail UseCase", () => {
     });
 
     it("should not be able to create a new e-mail with email already exists", async () => {
-        const author: ICreateAuthorDTO = {
-            id: faker.datatype.uuid(),
-            name: faker.name.fullName(),
-            whatsapp: faker.phone.number(),
-        };
-
-        const authorCreated = await createAuthorUseCase.execute(author);
-
         const email1: ICreateEmailDTO = {
             id: faker.datatype.uuid(),
             e_mail: "test@test.com.br",
-            author_id: authorCreated.id,
         };
 
         const email2: ICreateEmailDTO = {
             id: faker.datatype.uuid(),
             e_mail: "test@test.com.br",
-            author_id: authorCreated.id,
         };
 
-        const e_mail1 = await createEmailUseCase.execute(email1);
+        await createEmailUseCase.execute(email1);
 
         await expect(createEmailUseCase.execute(email2)).rejects.toEqual(
             new AppError("Email is already exists.", 401)
-        );
-    });
-
-    it("should not be able to create a new e-mail with author_id not found", async () => {
-        const email: ICreateEmailDTO = {
-            id: faker.datatype.uuid(),
-            e_mail: "test@test.com.br",
-            author_id: faker.datatype.uuid(),
-        };
-
-        await expect(createEmailUseCase.execute(email)).rejects.toEqual(
-            new AppError("Author not found", 404)
         );
     });
 });
