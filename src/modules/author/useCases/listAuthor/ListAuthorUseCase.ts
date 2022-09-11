8f7babb99db801f1842fd1e988b662f1bcafa568
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-11 08:55:54 -0300

    remove instances de emails e recipes e list author

--- src/modules/author/useCases/listAuthor/ListAuthorUseCase.ts
+++ src/modules/author/useCases/listAuthor/ListAuthorUseCase.ts
@@ -7,22 +7,8 @@ import { inject, injectable } from "tsyringe";
 @injectable()
 export class ListAuthorUseCase {
     constructor(
-        // author tem fk de emails e recipes
-        // instanciamos os repositorios, mas nao fazemos
-        // nenhuma operação, apenas para quando
-        // formos criar os test conseguirmos fazermos o leftjoin
-        // em emails e recipes, buscando cada email e recipe
-        // pelo author_id e atualizamos a lista de emails e recipes em author
-        // me memoria para retornar todos os relacionandos
-
         @inject("AuthorRepository")
-        private authorRepository: IAuthorRepository,
-
-        @inject("EmailRepository")
-        private emailRepository: IEmailRepository,
-
-        @inject("RecipeRepository")
-        private recipeRepository: IRecipeRepository
+        private authorRepository: IAuthorRepository
     ) {}
 
     async execute(): Promise<Author[]> {
