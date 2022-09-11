df189e795740a464d5be63faa691226dd520b02a
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-10 21:46:12 -0300

    add author_id obrigatorio ao criar email case

--- src/modules/author/useCases/createEmail/CreateEmailUseCase.ts
+++ src/modules/author/useCases/createEmail/CreateEmailUseCase.ts
@@ -1,5 +1,6 @@
 import { ICreateEmailDTO } from "@modules/author/dtos/ICreateEmailDTO";
 import { Email } from "@modules/author/infra/typeorm/entities/Email";
+import { IAuthorRepository } from "@modules/author/repositories/IAuthorRepository";
 import { IEmailRepository } from "@modules/author/repositories/IEmailRepository";
 import { inject, injectable } from "tsyringe";
 
@@ -9,18 +10,30 @@ import { AppError } from "@shared/errors/AppError";
 export class CreateEmailUseCase {
     constructor(
         @inject("EmailRepository")
-        private emailRepository: IEmailRepository
+        private emailRepository: IEmailRepository,
+
+        @inject("AuthorRepository")
+        private authorRepository: IAuthorRepository
     ) {}
 
-    async execute({ id, e_mail }: ICreateEmailDTO): Promise<Email> {
+    async execute({ id, e_mail, author_id }: ICreateEmailDTO): Promise<Email> {
         const emailValidator = await this.emailRepository.findByEmail(e_mail);
 
         if (emailValidator) {
             throw new AppError("Email is already exists.", 401);
         }
+
+        // buscando name de autor
+        const authorValidator = await this.authorRepository.findById(author_id);
+
+        // validando se author existe
+        if (!authorValidator) {
+            throw new AppError("Author not found.", 404);
+        }
         const email = await this.emailRepository.create({
             id,
             e_mail,
+            author_id,
         });
 
         return email;
