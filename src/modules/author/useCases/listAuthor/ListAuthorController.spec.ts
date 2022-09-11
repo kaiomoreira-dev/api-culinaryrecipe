6964421b08d4727dec10fa707231262401183d45
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-11 09:18:33 -0300

    alter list p/ retornar emails e recipes de author

--- src/modules/author/useCases/listAuthor/ListAuthorController.spec.ts
+++ src/modules/author/useCases/listAuthor/ListAuthorController.spec.ts
@@ -1,5 +1,8 @@
 import "dotenv/config";
 import { faker } from "@faker-js/faker";
+import { Author } from "@modules/author/infra/typeorm/entities/Author";
+import { Ingredient } from "@modules/recipe/infra/typeorm/entities/Ingredient";
+import { Produto } from "@modules/recipe/infra/typeorm/entities/Produto";
 import request from "supertest";
 import { DataSource } from "typeorm";
 
@@ -22,54 +25,89 @@ describe("List author Controller", () => {
     });
 
     it("should to list authors", async () => {
-        await request(app)
-            .post("/author")
+        const resposeCreateAuthor = await request(app).post("/author").send({
+            id: "86a5a72d-308a-41d2-a166-89966c738804",
+            name: "Kaio Moreira",
+            whatsapp: faker.phone.number(),
+        });
+
+        const { id: author_id } = resposeCreateAuthor.body as Author;
+
+        await request(app).post("/email").send({
+            id: faker.datatype.uuid(),
+            e_mail: faker.internet.email(),
+            author_id,
+        });
+
+        await request(app).post("/email").send({
+            id: faker.datatype.uuid(),
+            e_mail: faker.internet.email(),
+            author_id,
+        });
+
+        const resposeCreateProduto1 = await request(app).post("/produto").send({
+            id: faker.datatype.uuid(),
+            name: "Alho",
+            description: faker.lorem.paragraphs(),
+        });
+
+        const { id: produto_id1 } = resposeCreateProduto1.body as Produto;
+
+        const resposeCreateProduto2 = await request(app).post("/produto").send({
+            id: faker.datatype.uuid(),
+            name: "Cebola",
+            description: faker.lorem.paragraphs(),
+        });
+
+        const { id: produto_id2 } = resposeCreateProduto2.body as Produto;
+
+        const responseCreateIngredient1 = await request(app)
+            .post("/ingredient")
             .send({
-                id: "86a5a72d-308a-41d2-a166-89966c738804",
-                name: "Kaio Moreira",
-                whatsapp: faker.phone.number(),
-                emails: [
-                    {
-                        id: faker.datatype.uuid(),
-                        e_mail: faker.internet.email(),
-                        author_id: "86a5a72d-308a-41d2-a166-89966c738804",
-                        created_at: faker.datatype.datetime(),
-                        updated_at: faker.datatype.datetime(),
-                    },
-                ],
-                recipes: [
-                    {
-                        id: faker.datatype.uuid(),
-                        name: "Receita test 1",
-                        description: faker.lorem.paragraphs(),
-                        author_id: "86a5a72d-308a-41d2-a166-89966c738804",
-                        difficulty: "medium",
-                        dish_type: "appetizer",
-                        time: 5,
-                        total_guests: 5,
-                        additional_features: "low custom",
-                        ingredients: [
-                            {
-                                id: faker.datatype.uuid(),
-                                description: faker.lorem.paragraphs(),
-                                name: "alho roxo",
-                                produto_id:
-                                    "0458f9a7-e132-487e-ac1c-e072bfa1422d",
-                                unity: 1,
-                                weight: 100,
-                                created_at: String(faker.datatype.datetime()),
-                                updated_at: String(faker.datatype.datetime()),
-                            },
-                        ],
-                        created_at: faker.datatype.datetime(),
-                        updated_at: faker.datatype.datetime(),
-                    },
-                ],
+                id: faker.datatype.uuid(),
+                produto_id: produto_id1,
+                name: "Alho",
+                description: faker.lorem.paragraphs(),
+                unity: 1,
+                weight: 100,
+            });
+
+        const responseCreateIngredient2 = await request(app)
+            .post("/ingredient")
+            .send({
+                id: faker.datatype.uuid(),
+                produto_id: produto_id2,
+                name: "Cebola",
+                description: faker.lorem.paragraphs(),
+                unity: 1,
+                weight: 100,
+            });
+
+        const { id: ingredient_id1 } =
+            responseCreateIngredient1.body as Ingredient;
+        const { id: ingredient_id2 } =
+            responseCreateIngredient2.body as Ingredient;
+
+        const ingredients: string[] = [ingredient_id1, ingredient_id2];
+
+        const x = await request(app)
+            .post("/recipe")
+            .send({
+                id: faker.datatype.uuid(),
+                name: "Receita 1",
+                description: faker.lorem.words(10),
+                time: 20,
+                difficulty: "easy",
+                dish_type: "appetizer",
+                additional_features: "cheap dish",
+                total_guests: 5,
+                ingredients,
+                author_id,
             });
 
         const responseListAuthors = await request(app).get("/author").send({});
 
-        // console.log(JSON.stringify(responseAuthor.body, null, 2));
+        console.log(JSON.stringify(responseListAuthors.body, null, 2));
 
         expect(responseListAuthors.status).toBe(200);
     });
