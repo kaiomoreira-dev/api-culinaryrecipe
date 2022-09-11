8ed4b7246b87dbd908948bf5d7b78e4614c0b2d2
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-10 21:47:00 -0300

    add author_id ao criar email controller

--- src/modules/author/useCases/createEmail/CreateEmailController.ts
+++ src/modules/author/useCases/createEmail/CreateEmailController.ts
@@ -5,11 +5,11 @@ import { CreateEmailUseCase } from "./CreateEmailUseCase";
 
 export class CreateEmailController {
     async handle(request: Request, response: Response): Promise<Response> {
-        const { e_mail } = request.body;
+        const { e_mail, author_id } = request.body;
 
         const createEmailUseCase = container.resolve(CreateEmailUseCase);
 
-        const email = await createEmailUseCase.execute({ e_mail });
+        const email = await createEmailUseCase.execute({ e_mail, author_id });
 
         return response.status(200).json(email);
     }
