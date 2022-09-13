e6483e7eb1e7c0ad799aede9d481afce1f4e9015
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-13 17:22:22 -0300

    create ingredient test case c/ repositorys memory

---
+++ src/modules/recipe/useCases/createIngredient/CreateIngredientUseCase.spec.ts
@@ -0,0 +1,10 @@
+import { IngredientRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/IngredientRepositoryInMemory";
+import { ProdutoRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/ProdutoRepositoryInMemory";
+
+import { CreateProdutoUseCase } from "../createProduto/CreateProdutoUseCase";
+import { CreateIngredientUseCase } from "./CreateIngredientUseCase";
+
+let produtoRepositoryInMemory: ProdutoRepositoryInMemory;
+let ingredientRepositoryInMemory: IngredientRepositoryInMemory;
+let createIngredientUseCase: CreateIngredientUseCase;
+let createProdutoUseCase: CreateProdutoUseCase;
