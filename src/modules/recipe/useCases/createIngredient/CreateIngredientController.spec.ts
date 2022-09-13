97b58c1c7fc3405d8e7ab5cfad1a4f3b958dbf79
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-13 17:46:41 -0300

    create test p/ validar se ingredient ja existe

--- src/modules/recipe/useCases/createIngredient/CreateIngredientController.spec.ts
+++ src/modules/recipe/useCases/createIngredient/CreateIngredientController.spec.ts
@@ -48,4 +48,55 @@ describe("Create ingredient Controller", () => {
 
         expect(ingredient1.status).toBe(200);
     });
+    it("should not be able to create ingredient with produto_id invalid", async () => {
+        const produtoIdFake = faker.datatype.uuid();
+
+        const ingredient1 = await request(app)
+            .post("/ingredient")
+            .send({
+                id: faker.datatype.uuid(),
+                produto_id: produtoIdFake,
+                name: "Alho",
+                description: faker.lorem.words(20),
+                unity: 1,
+                weight: 100,
+            });
+
+        expect(ingredient1.status).toBe(401);
+    });
+    it("should not be able to create ingredient already exists", async () => {
+        const produto1 = await request(app)
+            .post("/produto")
+            .send({
+                id: faker.datatype.uuid(),
+                name: "Alho",
+                description: faker.lorem.words(20),
+            });
+
+        const { id: prodId1 } = produto1.body as Produto;
+
+        await request(app)
+            .post("/ingredient")
+            .send({
+                id: faker.datatype.uuid(),
+                produto_id: prodId1,
+                name: "Alho",
+                description: faker.lorem.words(20),
+                unity: 1,
+                weight: 100,
+            });
+
+        const ingredient1 = await request(app)
+            .post("/ingredient")
+            .send({
+                id: faker.datatype.uuid(),
+                produto_id: prodId1,
+                name: "Alho",
+                description: faker.lorem.words(20),
+                unity: 1,
+                weight: 100,
+            });
+
+        expect(ingredient1.status).toBe(401);
+    });
 });
