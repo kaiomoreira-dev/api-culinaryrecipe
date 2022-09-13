bb8d29f4042d6164a5e38b071961c544a68fb97d
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-13 17:27:45 -0300

    create test p/ validar criação de ingredient

--- src/modules/recipe/useCases/createIngredient/CreateIngredientUseCase.spec.ts
+++ src/modules/recipe/useCases/createIngredient/CreateIngredientUseCase.spec.ts
@@ -1,3 +1,6 @@
+import { faker } from "@faker-js/faker";
+import { ICreateIngredientDTO } from "@modules/recipe/dtos/ICreateIngredientDTO";
+import { ICreateProdutoDTO } from "@modules/recipe/dtos/ICreateProdutoDTO";
 import { IngredientRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/IngredientRepositoryInMemory";
 import { ProdutoRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/ProdutoRepositoryInMemory";
 
@@ -21,5 +24,30 @@ describe("Create ingredient UseCase", () => {
         createProdutoUseCase = new CreateProdutoUseCase(
             produtoRepositoryInMemory
         );
+    });
+
+    it("should be able to create ingredient", async () => {
+        const produto1: ICreateProdutoDTO = {
+            id: faker.datatype.uuid(),
+            name: "Alho",
+            description: faker.lorem.paragraphs(),
+        };
+
+        const createProduto1 = await createProdutoUseCase.execute(produto1);
+
+        const ingredient1: ICreateIngredientDTO = {
+            id: faker.datatype.uuid(),
+            description: faker.lorem.words(20),
+            name: "Alho",
+            produto_id: createProduto1.id,
+            unity: 1,
+            weight: 100,
+        };
+
+        const createIngredient1 = await createIngredientUseCase.execute(
+            ingredient1
+        );
+
+        expect(createIngredient1).toHaveProperty("id");
     });
 });
