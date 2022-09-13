c58d2722e49bf3f1e47355ffb5e2a410130c339e
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-13 18:12:59 -0300

    create test p/ verificar a listagem de ingredients

--- src/modules/recipe/useCases/listIngredient/ListIngredientUseCase.spec.ts
+++ src/modules/recipe/useCases/listIngredient/ListIngredientUseCase.spec.ts
@@ -1,15 +1,19 @@
+import { faker } from "@faker-js/faker";
+import { ICreateIngredientDTO } from "@modules/recipe/dtos/ICreateIngredientDTO";
+import { ICreateProdutoDTO } from "@modules/recipe/dtos/ICreateProdutoDTO";
 import { IngredientRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/IngredientRepositoryInMemory";
 import { ProdutoRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/ProdutoRepositoryInMemory";
 
 import { CreateIngredientUseCase } from "../createIngredient/CreateIngredientUseCase";
 import { CreateProdutoUseCase } from "../createProduto/CreateProdutoUseCase";
 import { ListProdutosUseCase } from "../listProdutos/ListProdutosUseCase";
+import { ListIngredientUseCase } from "./ListIngredientUseCase";
 
 let produtoRepositoryInMemory: ProdutoRepositoryInMemory;
 let ingredientRepositoryInMemory: IngredientRepositoryInMemory;
 let createIngredientUseCase: CreateIngredientUseCase;
 let createProdutoUseCase: CreateProdutoUseCase;
-let listProdutoUseCase: ListProdutosUseCase;
+let listIngredientUseCase: ListIngredientUseCase;
 
 describe("List ingredients UseCase", () => {
     beforeEach(() => {
@@ -23,6 +27,51 @@ describe("List ingredients UseCase", () => {
         createProdutoUseCase = new CreateProdutoUseCase(
             produtoRepositoryInMemory
         );
-        listProdutoUseCase = new ListProdutosUseCase(produtoRepositoryInMemory);
+        listIngredientUseCase = new ListIngredientUseCase(
+            ingredientRepositoryInMemory
+        );
+    });
+
+    it("should be able to list ingredients", async () => {
+        const produto1: ICreateProdutoDTO = {
+            id: faker.datatype.uuid(),
+            name: "Alho",
+            description: faker.lorem.paragraphs(),
+        };
+        const produto2: ICreateProdutoDTO = {
+            id: faker.datatype.uuid(),
+            name: "Cebola",
+            description: faker.lorem.paragraphs(),
+        };
+
+        const createProduto1 = await createProdutoUseCase.execute(produto1);
+
+        const createProduto2 = await createProdutoUseCase.execute(produto2);
+
+        const ingredient1: ICreateIngredientDTO = {
+            id: faker.datatype.uuid(),
+            description: faker.lorem.words(20),
+            name: "Alho",
+            produto_id: createProduto1.id,
+            unity: 1,
+            weight: 100,
+        };
+
+        const ingredient2: ICreateIngredientDTO = {
+            id: faker.datatype.uuid(),
+            description: faker.lorem.words(20),
+            name: "Cebola",
+            produto_id: createProduto2.id,
+            unity: 1,
+            weight: 100,
+        };
+
+        await createIngredientUseCase.execute(ingredient1);
+
+        await createIngredientUseCase.execute(ingredient2);
+
+        const listIngredients = await listIngredientUseCase.execute();
+
+        expect(listIngredients[0]).toHaveProperty("id");
     });
 });
