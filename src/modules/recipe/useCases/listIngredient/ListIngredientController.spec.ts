74f8e8a477655e072100562d2fec3d28181252aa
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-13 18:13:55 -0300

    import configs p/ test controller list ingredient

---
+++ src/modules/recipe/useCases/listIngredient/ListIngredientController.spec.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import { faker } from "@faker-js/faker";
+import { Author } from "@modules/author/infra/typeorm/entities/Author";
+import { Ingredient } from "@modules/recipe/infra/typeorm/entities/Ingredient";
+import { Produto } from "@modules/recipe/infra/typeorm/entities/Produto";
+import request from "supertest";
+import { DataSource } from "typeorm";
+
+import { app } from "@shared/infra/http/app";
+import { createConnection } from "@shared/infra/typeorm";
