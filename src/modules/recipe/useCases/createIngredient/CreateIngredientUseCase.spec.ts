2aafed74ac7a45989d19bd2e9fc402d4eb32eb72
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-13 17:24:12 -0300

    create beforeach c/ instance dos repository memory

--- src/modules/recipe/useCases/createIngredient/CreateIngredientUseCase.spec.ts
+++ src/modules/recipe/useCases/createIngredient/CreateIngredientUseCase.spec.ts
@@ -9,4 +9,17 @@ let ingredientRepositoryInMemory: IngredientRepositoryInMemory;
 let createIngredientUseCase: CreateIngredientUseCase;
 let createProdutoUseCase: CreateProdutoUseCase;
 
-describe("Create ingredient UseCase", () => {});
+describe("Create ingredient UseCase", () => {
+    beforeEach(() => {
+        produtoRepositoryInMemory = new ProdutoRepositoryInMemory();
+        ingredientRepositoryInMemory = new IngredientRepositoryInMemory();
+
+        createIngredientUseCase = new CreateIngredientUseCase(
+            ingredientRepositoryInMemory,
+            produtoRepositoryInMemory
+        );
+        createProdutoUseCase = new CreateProdutoUseCase(
+            produtoRepositoryInMemory
+        );
+    });
+});
