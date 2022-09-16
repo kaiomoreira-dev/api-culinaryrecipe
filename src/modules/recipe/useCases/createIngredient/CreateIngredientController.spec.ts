ceb5e3ba0e69fb6bd8873ac8c2650d8360a0d19b
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-16 14:32:14 -0300

    remove importações nao utilizadas

--- src/modules/recipe/useCases/createIngredient/CreateIngredientController.spec.ts
+++ src/modules/recipe/useCases/createIngredient/CreateIngredientController.spec.ts
@@ -1,7 +1,5 @@
 import "dotenv/config";
 import { faker } from "@faker-js/faker";
-import { Author } from "@modules/author/infra/typeorm/entities/Author";
-import { Ingredient } from "@modules/recipe/infra/typeorm/entities/Ingredient";
 import { Produto } from "@modules/recipe/infra/typeorm/entities/Produto";
 import request from "supertest";
 import { DataSource } from "typeorm";
