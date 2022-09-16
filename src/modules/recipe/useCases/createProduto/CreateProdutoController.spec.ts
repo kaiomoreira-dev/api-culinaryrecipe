2b51d67cef9919bbb7e1c76b461c4a43540d5a0f
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-16 15:14:57 -0300

    create test p/ validar criação de produto

--- src/modules/recipe/useCases/createProduto/CreateProdutoController.spec.ts
+++ src/modules/recipe/useCases/createProduto/CreateProdutoController.spec.ts
@@ -4,6 +4,7 @@ import { Produto } from "@modules/recipe/infra/typeorm/entities/Produto";
 import request from "supertest";
 import { DataSource } from "typeorm";
 
+import { app } from "@shared/infra/http/app";
 import { createConnection } from "@shared/infra/typeorm";
 
 let connection: DataSource;
@@ -20,5 +21,17 @@ describe("Create produto Controller", () => {
         await connection.dropDatabase();
 
         await connection.destroy();
+    });
+
+    it("should be able to create produto", async () => {
+        const produto1 = await request(app)
+            .post("/produto")
+            .send({
+                id: faker.datatype.uuid(),
+                name: "Alho",
+                description: faker.lorem.words(20),
+            });
+
+        expect(produto1.status).toBe(200);
     });
 });
