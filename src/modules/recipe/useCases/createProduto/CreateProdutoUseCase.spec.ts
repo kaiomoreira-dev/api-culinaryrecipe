32f627069003ed48070ee5c0fc1b1b50c8e27c30
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-16 14:24:29 -0300

    create test p/ validat criação de produto

--- src/modules/recipe/useCases/createProduto/CreateProdutoUseCase.spec.ts
+++ src/modules/recipe/useCases/createProduto/CreateProdutoUseCase.spec.ts
@@ -1,3 +1,5 @@
+import { faker } from "@faker-js/faker";
+import { ICreateProdutoDTO } from "@modules/recipe/dtos/ICreateProdutoDTO";
 import { ProdutoRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/ProdutoRepositoryInMemory";
 
 import { CreateProdutoUseCase } from "./CreateProdutoUseCase";
@@ -12,5 +14,17 @@ describe("Create produto UseCase", () => {
         createProdutoUseCase = new CreateProdutoUseCase(
             produtoRepositoryInMemory
         );
+    });
+
+    it("should be able to create produto", async () => {
+        const produto: ICreateProdutoDTO = {
+            id: faker.datatype.uuid(),
+            name: "Alho",
+            description: faker.lorem.paragraphs(),
+        };
+
+        const createProduto = await createProdutoUseCase.execute(produto);
+
+        expect(createProduto).toHaveProperty("id");
     });
 });
