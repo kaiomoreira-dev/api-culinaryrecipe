80a16ee27ec071c48092739035678feb8cb35163
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-16 14:30:43 -0300

    create test p/ validar se produto ja existe

--- src/modules/recipe/useCases/createProduto/CreateProdutoUseCase.spec.ts
+++ src/modules/recipe/useCases/createProduto/CreateProdutoUseCase.spec.ts
@@ -2,6 +2,8 @@ import { faker } from "@faker-js/faker";
 import { ICreateProdutoDTO } from "@modules/recipe/dtos/ICreateProdutoDTO";
 import { ProdutoRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/ProdutoRepositoryInMemory";
 
+import { AppError } from "@shared/errors/AppError";
+
 import { CreateProdutoUseCase } from "./CreateProdutoUseCase";
 
 let produtoRepositoryInMemory: ProdutoRepositoryInMemory;
@@ -26,5 +28,25 @@ describe("Create produto UseCase", () => {
         const createProduto = await createProdutoUseCase.execute(produto);
 
         expect(createProduto).toHaveProperty("id");
+    });
+
+    it("should not be able to create produto is already exists", async () => {
+        const produto1: ICreateProdutoDTO = {
+            id: faker.datatype.uuid(),
+            name: "Alho",
+            description: faker.lorem.paragraphs(),
+        };
+
+        const produto2: ICreateProdutoDTO = {
+            id: faker.datatype.uuid(),
+            name: "Alho",
+            description: faker.lorem.paragraphs(),
+        };
+
+        await createProdutoUseCase.execute(produto1);
+
+        await expect(createProdutoUseCase.execute(produto2)).rejects.toEqual(
+            new AppError("Produto already exists", 401)
+        );
     });
 });
