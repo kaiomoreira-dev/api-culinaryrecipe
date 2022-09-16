dcb0e5bd3c4a87296968b519d3d7e938608ca7fa
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-16 15:33:37 -0300

    create test p/ validar a list c/ toods produtos

--- src/modules/recipe/useCases/listProdutos/ListProdutosController.spec.ts
+++ src/modules/recipe/useCases/listProdutos/ListProdutosController.spec.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import { faker } from "@faker-js/faker";
+import { Produto } from "@modules/recipe/infra/typeorm/entities/Produto";
 import request from "supertest";
 import { DataSource } from "typeorm";
 
@@ -19,5 +20,27 @@ describe("List produtos Controller", () => {
         await connection.dropDatabase();
 
         await connection.destroy();
+    });
+
+    it("should be able to list produtos", async () => {
+        await request(app)
+            .post("/produto")
+            .send({
+                id: faker.datatype.uuid(),
+                name: "Alho",
+                description: faker.lorem.words(20),
+            });
+
+        await request(app)
+            .post("/produto")
+            .send({
+                id: faker.datatype.uuid(),
+                name: "Alho",
+                description: faker.lorem.words(20),
+            });
+
+        const listProdutos = await request(app).get("/produto").send();
+
+        expect(listProdutos.status).toBe(200);
     });
 });
