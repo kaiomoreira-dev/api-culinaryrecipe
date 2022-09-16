14f00be090c5060de6c43755b4b6540403f2f1dc
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-16 15:19:22 -0300

    import repo e case de produto p/ test no list

---
+++ src/modules/recipe/useCases/listProdutos/ListProdutosUseCase.spec.ts
@@ -0,0 +1,12 @@
+import { faker } from "@faker-js/faker";
+import { ICreateProdutoDTO } from "@modules/recipe/dtos/ICreateProdutoDTO";
+import { ProdutoRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/ProdutoRepositoryInMemory";
+
+import { AppError } from "@shared/errors/AppError";
+
+import { CreateProdutoUseCase } from "../createProduto/CreateProdutoUseCase";
+import { ListProdutosUseCase } from "./ListProdutosUseCase";
+
+let produtoRepositoryInMemory: ProdutoRepositoryInMemory;
+let createProdutoUseCase: CreateProdutoUseCase;
+let listProdutosUseCase: ListProdutosUseCase;
