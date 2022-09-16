bbbef7fa1c372690210ce0aed5799423fb284d7b
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-16 14:36:17 -0300

    create beforEach p/ criar conex e migration

--- src/modules/recipe/useCases/createProduto/CreateProdutoController.spec.ts
+++ src/modules/recipe/useCases/createProduto/CreateProdutoController.spec.ts
@@ -4,6 +4,14 @@ import { Produto } from "@modules/recipe/infra/typeorm/entities/Produto";
 import request from "supertest";
 import { DataSource } from "typeorm";
 
+import { createConnection } from "@shared/infra/typeorm";
+
 let connection: DataSource;
 
-describe("Create produto Controller", () => {});
+describe("Create produto Controller", () => {
+    beforeAll(async () => {
+        connection = await createConnection("localhost");
+
+        await connection.runMigrations();
+    });
+});
