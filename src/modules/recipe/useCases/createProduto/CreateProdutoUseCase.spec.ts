0a699413928c47dbc0e09614fa6b10e7c335e641
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-16 14:20:02 -0300

    import repo e case de produto p test no case

---
+++ src/modules/recipe/useCases/createProduto/CreateProdutoUseCase.spec.ts
@@ -0,0 +1,6 @@
+import { ProdutoRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/ProdutoRepositoryInMemory";
+
+import { CreateProdutoUseCase } from "./CreateProdutoUseCase";
+
+let produtoRepositoryInMemory: ProdutoRepositoryInMemory;
+let createProdutoUseCase: CreateProdutoUseCase;
