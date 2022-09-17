3246c5a5b8bf52fa4a6e5b9d871cfb5e80a9ebc3
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-17 16:32:28 -0300

    alter find email controller p/ route params

--- src/modules/author/useCases/findEmail/FindEmailController.ts
+++ src/modules/author/useCases/findEmail/FindEmailController.ts
@@ -5,13 +5,11 @@ import { FindEmailUseCase } from "./FindEmailUseCase";
 
 export class FindEmailController {
     async handle(request: Request, response: Response): Promise<Response> {
-        const { email: e_mail } = request.query;
-
-        const emailFormat = String(e_mail);
+        const { id } = request.params;
 
         const findEmailUseCase = container.resolve(FindEmailUseCase);
 
-        const email = await findEmailUseCase.execute(emailFormat);
+        const email = await findEmailUseCase.execute(id);
 
         return response.status(200).json(email);
     }
