86aca24e6ea7d9ce5e2ac312ee7f87c3f2253d45
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-17 17:43:15 -0300

    alter controller find author p/ route params

--- src/modules/author/useCases/findAuthor/FindAuthorIdController.ts
+++ src/modules/author/useCases/findAuthor/FindAuthorIdController.ts
@@ -5,11 +5,10 @@ import { FindAuthorIdUseCase } from "./FindAuthorIdUseCase";
 
 export class FindAuthorIdController {
     async handle(request: Request, response: Response): Promise<Response> {
-        const { author_id } = request.body;
+        const { id } = request.params;
+        const findAuthorById = container.resolve(FindAuthorIdUseCase);
 
-        const findAuthorByName = container.resolve(FindAuthorIdUseCase);
-
-        const author = await findAuthorByName.execute(author_id);
+        const author = await findAuthorById.execute(id);
 
         return response.status(200).json(author);
     }
