d347a6963c2ba4e38566717c8dfaf0789d1b4ac3
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-17 19:14:56 -0300

    alter request.body id p/ params

--- src/modules/author/useCases/deleteAuthor/DeleteAuthorController.ts
+++ src/modules/author/useCases/deleteAuthor/DeleteAuthorController.ts
@@ -5,7 +5,7 @@ import { DeleteAuthorUseCase } from "./DeleteAuthorUseCase";
 
 export class DeleteAuthorController {
     async handle(request: Request, response: Response): Promise<Response> {
-        const { id } = request.body;
+        const { id } = request.params;
 
         const deleteAuthorUseCase = container.resolve(DeleteAuthorUseCase);
 
