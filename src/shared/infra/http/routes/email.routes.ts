26ea27ce728fe7780b1450f3f146855fe2ab37c3
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-17 15:41:44 -0300

    remove email case inrregular

--- src/shared/infra/http/routes/email.routes.ts
+++ src/shared/infra/http/routes/email.routes.ts
@@ -1,6 +1,5 @@
 import { CreateEmailController } from "@modules/author/useCases/createEmail/CreateEmailController";
 import { DeleteEmailController } from "@modules/author/useCases/deleteEmail/DeleteEmailController";
-import { FindEmailController } from "@modules/author/useCases/findEmail/FindEmailController";
 import { ListEmailsController } from "@modules/author/useCases/listEmails/ListEmailsController";
 import { UpdateE_mailByE_mailController } from "@modules/author/useCases/updateEmail/UpdateE_mailByE_mailController";
 import { Router } from "express";
@@ -11,8 +10,6 @@ const createEmailController = new CreateEmailController();
 
 const listEmailsController = new ListEmailsController();
 
-const findEmailController = new FindEmailController();
-
 const deleteEmailController = new DeleteEmailController();
 
 const updateE_mailByE_mailController = new UpdateE_mailByE_mailController();
@@ -20,8 +17,6 @@ const updateE_mailByE_mailController = new UpdateE_mailByE_mailController();
 emailRoutes.post("/", createEmailController.handle);
 
 emailRoutes.get("/", listEmailsController.handle);
-
-emailRoutes.get("/find", findEmailController.handle);
 
 emailRoutes.delete("/delete", deleteEmailController.handle);
 
