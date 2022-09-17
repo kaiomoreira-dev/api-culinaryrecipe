7ca04e05213396240e901ea86e0542e1b5652b25
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-17 19:11:15 -0300

    alter file p/ findAuthorById

---
+++ src/modules/author/useCases/findAuthorById/FindAuthorIdController.spec.ts
@@ -0,0 +1,49 @@
+import "dotenv/config";
+import { faker } from "@faker-js/faker";
+import { Author } from "@modules/author/infra/typeorm/entities/Author";
+import request from "supertest";
+import { DataSource } from "typeorm";
+
+import { app } from "@shared/infra/http/app";
+import { createConnection } from "@shared/infra/typeorm";
+
+let connection: DataSource;
+
+describe("Find author Controller", () => {
+    beforeAll(async () => {
+        connection = await createConnection("localhost");
+
+        await connection.runMigrations();
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+
+        await connection.destroy();
+    });
+
+    it("should be able to find author using id", async () => {
+        const responseAuthor = await request(app).post("/author").send({
+            id: faker.datatype.uuid(),
+            name: "Kaio Moreira",
+            whatsapp: faker.phone.number(),
+        });
+
+        const { id } = responseAuthor.body as Author;
+
+        const responseFindAuthor = await request(app)
+            .get(`/author/find/${id}`)
+            .send();
+        expect(responseFindAuthor.status).toBe(200);
+    });
+
+    it("should not be able to find author using id invalid", async () => {
+        const id = "f060c7bc-4539-4bd8-a3ac-f4b7d98f54d6";
+
+        const responseFindAuthor = await request(app)
+            .get(`/author/find/${id}`)
+            .send();
+
+        expect(responseFindAuthor.status).toBe(404);
+    });
+});
