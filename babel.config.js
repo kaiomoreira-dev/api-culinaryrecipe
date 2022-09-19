31c35e71bca9e7c90614429ca2f7ad0f6b61e393
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-19 14:31:41 -0300

    config build c/ babel

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    presets: [
+      ["@babel/preset-env", { targets: { node: "current" } }],
+      "@babel/preset-typescript",
+    ],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@modules": "./src/modules",
+            "@shared": "./src/shared",
+            "@erros": "./src/errors",
+            "@config": "./src/config",
+            "@utils": "./src/utils",
+          },
+        },
+      ],
+      "babel-plugin-transform-typescript-metadata",
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
+      ["@babel/plugin-proposal-class-properties", { loose: true }],
+    ],
+  };
+  
