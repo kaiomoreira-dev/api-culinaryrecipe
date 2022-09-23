481311e76c0c4d3f118ff08ec9bb8da662c63243
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-23 17:49:46 -0300

    Create route get ListIngredientByProduto

--- src/shared/infra/http/routes/ingredient.routes.ts
+++ src/shared/infra/http/routes/ingredient.routes.ts
@@ -1,5 +1,6 @@
 import { CreateIngredientController } from "@modules/recipe/useCases/createIngredient/CreateIngredientController";
 import { ListIngredientController } from "@modules/recipe/useCases/listIngredient/ListIngredientController";
+import { ListIngredientByProdutoController } from "@modules/recipe/useCases/listIngredientByProduto/ListIngredientByProdutoController";
 import { Router } from "express";
 
 export const ingredientRoutes = Router();
@@ -8,6 +9,11 @@ const createIngredientController = new CreateIngredientController();
 
 const listIngredientController = new ListIngredientController();
 
+const listIngredientByProdutoController =
+  new ListIngredientByProdutoController();
+
 ingredientRoutes.post("/", createIngredientController.handle);
 
 ingredientRoutes.get("/", listIngredientController.handle);
+
+ingredientRoutes.get("/produto/:id", listIngredientByProdutoController.handle);
