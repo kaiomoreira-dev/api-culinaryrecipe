405147a19468ffac875cced9ae614f93737e641d
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-22 23:18:59 -0300

    Create test p/ validar update controller produto

--- src/modules/recipe/useCases/createProduto/CreateProdutoController.spec.ts
+++ src/modules/recipe/useCases/createProduto/CreateProdutoController.spec.ts
@@ -9,48 +9,48 @@ import { createConnection } from "@shared/infra/typeorm";
 let connection: DataSource;
 
 describe("Create produto Controller", () => {
-    beforeAll(async () => {
-        connection = await createConnection("localhost");
+  beforeAll(async () => {
+    connection = await createConnection("localhost");
 
-        await connection.runMigrations();
-    });
+    await connection.runMigrations();
+  });
 
-    afterAll(async () => {
-        // dropbase
-        await connection.dropDatabase();
+  afterAll(async () => {
+    // dropbase
+    await connection.dropDatabase();
 
-        await connection.destroy();
-    });
+    await connection.destroy();
+  });
 
-    it("should be able to create produto", async () => {
-        const produto1 = await request(app)
-            .post("/produto")
-            .send({
-                id: faker.datatype.uuid(),
-                name: "Alho",
-                description: faker.lorem.words(20),
-            });
+  it("should be able to create produto", async () => {
+    const produto1 = await request(app)
+      .post("/produto")
+      .send({
+        id: faker.datatype.uuid(),
+        name: "Alho",
+        description: faker.lorem.words(20),
+      });
 
-        expect(produto1.status).toBe(200);
-    });
+    expect(produto1.status).toBe(200);
+  });
 
-    it("should not be able to create produto already exists", async () => {
-        await request(app)
-            .post("/produto")
-            .send({
-                id: faker.datatype.uuid(),
-                name: "Alho",
-                description: faker.lorem.words(20),
-            });
+  it("should not be able to create produto already exists", async () => {
+    await request(app)
+      .post("/produto")
+      .send({
+        id: faker.datatype.uuid(),
+        name: "Alho",
+        description: faker.lorem.words(20),
+      });
 
-        const produto2 = await request(app)
-            .post("/produto")
-            .send({
-                id: faker.datatype.uuid(),
-                name: "Alho",
-                description: faker.lorem.words(20),
-            });
+    const produto2 = await request(app)
+      .post("/produto")
+      .send({
+        id: faker.datatype.uuid(),
+        name: "Alho",
+        description: faker.lorem.words(20),
+      });
 
-        expect(produto2.status).toBe(401);
-    });
+    expect(produto2.status).toBe(401);
+  });
 });
