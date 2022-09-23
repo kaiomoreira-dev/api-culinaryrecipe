3446dc0480918941d5609d694ad6cf2f9bdcb50e
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-23 00:05:08 -0300

    add if p/ validar NODE_ENV diferente test em app

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -30,20 +30,22 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(cors());
 
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  integrations: [
-    // enable HTTP calls tracing
-    new Sentry.Integrations.Http({ tracing: true }),
-    // enable Express.js middleware tracing
-    new Tracing.Integrations.Express({ app }),
-  ],
+if (process.env.NODE_ENV !== "test") {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // enable Express.js middleware tracing
+      new Tracing.Integrations.Express({ app }),
+    ],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 // The request handler must be the first middleware on the app
 app.use(Sentry.Handlers.requestHandler());
