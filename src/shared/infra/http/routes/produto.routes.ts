e2f4633a3711ae5ceae0d7c19be4f0ff501d8ff3
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-22 23:16:34 -0300

    create rota put p/ atualizar produto

--- src/shared/infra/http/routes/produto.routes.ts
+++ src/shared/infra/http/routes/produto.routes.ts
@@ -1,5 +1,6 @@
 import { CreateProdutoController } from "@modules/recipe/useCases/createProduto/CreateProdutoController";
 import { ListProdutosController } from "@modules/recipe/useCases/listProdutos/ListProdutosController";
+import { UpdateProdutoController } from "@modules/recipe/useCases/updateProduto/UpdateProdutoController";
 import { Router } from "express";
 
 export const produtoRoutes = Router();
@@ -8,6 +9,10 @@ const createProdutoController = new CreateProdutoController();
 
 const listProdutosController = new ListProdutosController();
 
+const updateProdutoController = new UpdateProdutoController();
+
 produtoRoutes.post("/", createProdutoController.handle);
 
 produtoRoutes.get("/", listProdutosController.handle);
+
+produtoRoutes.put("/:id", updateProdutoController.handle);
