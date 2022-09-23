28436ca4031cdc46c9026e938fb0a6c032802760
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-22 21:09:52 -0300

    create test p/ validar id ao atualizar produto

--- src/modules/recipe/useCases/updateProduto/UpdateProdutoUseCase.spec.ts
+++ src/modules/recipe/useCases/updateProduto/UpdateProdutoUseCase.spec.ts
@@ -2,6 +2,7 @@ import { faker } from "@faker-js/faker";
 import { ICreateProdutoDTO } from "@modules/recipe/dtos/ICreateProdutoDTO";
 import { ProdutoRepositoryInMemory } from "@modules/recipe/repositories/in-Memory/ProdutoRepositoryInMemory";
 
+import { AppError } from "@shared/errors/AppError";
 import { redisClient } from "@shared/infra/http/middlewares/rateLimiter";
 
 import { CreateProdutoUseCase } from "../createProduto/CreateProdutoUseCase";
@@ -36,5 +37,13 @@ describe("Update produto UseCase", () => {
     const updateProduto = await updateProdutoUseCase.execute(createProduto.id);
 
     expect(updateProduto).toHaveProperty("id");
+  });
+
+  it("should not be able to update produto not exists", async () => {
+    const produtoIdFake = faker.datatype.uuid();
+
+    await expect(updateProdutoUseCase.execute(produtoIdFake)).rejects.toEqual(
+      new AppError("Produto not found", 404)
+    );
   });
 });
