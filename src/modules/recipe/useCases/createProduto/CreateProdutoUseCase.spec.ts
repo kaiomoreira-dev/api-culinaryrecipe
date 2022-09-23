cb258a6bcb4ad153390f3a4405a6e31a783cef4f
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-22 21:05:21 -0300

    remove sujeiras em createProdutoUseCase.spec

--- src/modules/recipe/useCases/createProduto/CreateProdutoUseCase.spec.ts
+++ src/modules/recipe/useCases/createProduto/CreateProdutoUseCase.spec.ts
@@ -11,47 +11,45 @@ let produtoRepositoryInMemory: ProdutoRepositoryInMemory;
 let createProdutoUseCase: CreateProdutoUseCase;
 
 describe("Create produto UseCase", () => {
-    beforeEach(() => {
-        produtoRepositoryInMemory = new ProdutoRepositoryInMemory();
+  beforeEach(() => {
+    produtoRepositoryInMemory = new ProdutoRepositoryInMemory();
 
-        createProdutoUseCase = new CreateProdutoUseCase(
-            produtoRepositoryInMemory
-        );
-    });
+    createProdutoUseCase = new CreateProdutoUseCase(produtoRepositoryInMemory);
+  });
 
-    afterAll(() => {
-        redisClient.quit();
-    });
+  afterAll(() => {
+    redisClient.quit();
+  });
 
-    it("should be able to create produto", async () => {
-        const produto: ICreateProdutoDTO = {
-            id: faker.datatype.uuid(),
-            name: "Alho",
-            description: faker.lorem.paragraphs(),
-        };
+  it("should be able to create produto", async () => {
+    const produto: ICreateProdutoDTO = {
+      id: faker.datatype.uuid(),
+      name: "Alho",
+      description: faker.lorem.paragraphs(),
+    };
 
-        const createProduto = await createProdutoUseCase.execute(produto);
+    const createProduto = await createProdutoUseCase.execute(produto);
 
-        expect(createProduto).toHaveProperty("id");
-    });
+    expect(createProduto).toHaveProperty("id");
+  });
 
-    it("should not be able to create produto is already exists", async () => {
-        const produto1: ICreateProdutoDTO = {
-            id: faker.datatype.uuid(),
-            name: "Alho",
-            description: faker.lorem.paragraphs(),
-        };
+  it("should not be able to create produto is already exists", async () => {
+    const produto1: ICreateProdutoDTO = {
+      id: faker.datatype.uuid(),
+      name: "Alho",
+      description: faker.lorem.paragraphs(),
+    };
 
-        const produto2: ICreateProdutoDTO = {
-            id: faker.datatype.uuid(),
-            name: "Alho",
-            description: faker.lorem.paragraphs(),
-        };
+    const produto2: ICreateProdutoDTO = {
+      id: faker.datatype.uuid(),
+      name: "Alho",
+      description: faker.lorem.paragraphs(),
+    };
 
-        await createProdutoUseCase.execute(produto1);
+    await createProdutoUseCase.execute(produto1);
 
-        await expect(createProdutoUseCase.execute(produto2)).rejects.toEqual(
-            new AppError("Produto already exists", 401)
-        );
-    });
+    await expect(createProdutoUseCase.execute(produto2)).rejects.toEqual(
+      new AppError("Produto already exists", 401)
+    );
+  });
 });
