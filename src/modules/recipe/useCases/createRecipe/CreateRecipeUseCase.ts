d3858d8161b090a40e20fa6dc9f0607ed73da4c0
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-24 22:37:10 -0300

    remove sujeiras de importações

--- src/modules/recipe/useCases/createRecipe/CreateRecipeUseCase.ts
+++ src/modules/recipe/useCases/createRecipe/CreateRecipeUseCase.ts
@@ -13,85 +13,83 @@ import { AppError } from "@shared/errors/AppError";
 
 @injectable()
 export class CreateRecipeUseCase {
-    constructor(
-        @inject("RecipeRepository")
-        private recipeRepository: IRecipeRepository,
+  constructor(
+    @inject("RecipeRepository")
+    private recipeRepository: IRecipeRepository,
 
-        @inject("IngredientRepository")
-        private ingredientRepository: IIngredientRepository,
+    @inject("IngredientRepository")
+    private ingredientRepository: IIngredientRepository,
 
-        @inject("AuthorRepository")
-        private authorRepository: IAuthorRepository
-    ) {}
+    @inject("AuthorRepository")
+    private authorRepository: IAuthorRepository
+  ) {}
 
-    async execute(
-        {
-            id,
-            name,
-            description,
-            difficulty,
-            dish_type,
-            additional_features,
-            time,
-            total_guests,
-            author_id,
-        }: ICreateRecipeDTO,
-        ingredients: string[]
-    ): Promise<Recipe> {
-        if (difficulty !== "easy" && "medium" && "hard") {
-            throw new AppError("difficulty incorrect!", 401);
-        }
-
-        if (dish_type !== "appetizer" && "main course" && "dessert") {
-            throw new AppError("dish_type incorrect!", 401);
-        }
-
-        // buscando name de autor
-        const authorValidator = await this.authorRepository.findById(author_id);
-
-        // validando se author existe
-        if (!authorValidator) {
-            throw new AppError("Author not found.", 404);
-        }
-
-        // array vazio de Ingredient
-        let addIngredients: Ingredient[] = [];
-
-        // enviando Ingredient para o array addIngredients
-        // sem utilizar async!!
-        for (const id of ingredients) {
-            // busca ingredient pelo nome
-            const foundIngredient = await this.ingredientRepository.findById(
-                id
-            );
-
-            if (!foundIngredient) {
-                throw new AppError("Ingredient not found", 404);
-            }
-
-            // recebe Ingredient buscado pelo nome para
-            // o array de Ingredient[]
-            addIngredients.push(foundIngredient);
-        }
-
-        if (addIngredients.length < 2) {
-            throw new AppError("Ingredients insufficient!", 401);
-        }
-
-        // criando recipe
-        const recipe = await this.recipeRepository.create({
-            id,
-            name,
-            description,
-            difficulty,
-            dish_type,
-            additional_features,
-            time,
-            total_guests,
-            author_id,
-            ingredients: addIngredients,
-        });
-
-        return recipe;
+  async execute(
+    {
+      id,
+      name,
+      description,
+      difficulty,
+      dish_type,
+      additional_features,
+      time,
+      total_guests,
+      author_id,
+    }: ICreateRecipeDTO,
+    ingredients: string[]
+  ): Promise<Recipe> {
+    if (difficulty !== "easy" && "medium" && "hard") {
+      throw new AppError("difficulty incorrect!", 401);
     }
+
+    if (dish_type !== "appetizer" && "main course" && "dessert") {
+      throw new AppError("dish_type incorrect!", 401);
+    }
+
+    // buscando name de autor
+    const authorValidator = await this.authorRepository.findById(author_id);
+
+    // validando se author existe
+    if (!authorValidator) {
+      throw new AppError("Author not found.", 404);
+    }
+
+    // array vazio de Ingredient
+    let addIngredients: Ingredient[] = [];
+
+    // enviando Ingredient para o array addIngredients
+    // sem utilizar async!!
+    for (const id of ingredients) {
+      // busca ingredient pelo nome
+      const foundIngredient = await this.ingredientRepository.findById(id);
+
+      if (!foundIngredient) {
+        throw new AppError("Ingredient not found", 404);
+      }
+
+      // recebe Ingredient buscado pelo nome para
+      // o array de Ingredient[]
+      addIngredients.push(foundIngredient);
+    }
+
+    if (addIngredients.length < 2) {
+      throw new AppError("Ingredients insufficient!", 401);
+    }
+
+    // criando recipe
+    const recipe = await this.recipeRepository.create({
+      id,
+      name,
+      description,
+      difficulty,
+      dish_type,
+      additional_features,
+      time,
+      total_guests,
+      author_id,
+      ingredients: addIngredients,
+    });
+
+    return recipe;
+  }
 }
