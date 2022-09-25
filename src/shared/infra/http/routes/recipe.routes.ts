62ae5d19962d18d74e91c7bc10c658437014de49
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-24 23:52:06 -0300

    Create route delete p/ deletar recipe p/ id params

--- src/shared/infra/http/routes/recipe.routes.ts
+++ src/shared/infra/http/routes/recipe.routes.ts
@@ -1,4 +1,5 @@
 import { CreateRecipeController } from "@modules/recipe/useCases/createRecipe/CreateRecipeController";
+import { DeleteRecipeController } from "@modules/recipe/useCases/deleteRecipe/DeleteRecipeController";
 import { ListRecipecontroller } from "@modules/recipe/useCases/listRecipe/ListRecipeController";
 import { ListRecipesByIngredientController } from "@modules/recipe/useCases/recipesIngredients/listByIngredient/ListRecipesByIngredientController";
 import { Router } from "express";
@@ -12,9 +13,13 @@ const listRecipecontroller = new ListRecipecontroller();
 const listRecipesByIngredientController =
   new ListRecipesByIngredientController();
 
+const deleteRecipeController = new DeleteRecipeController();
+
 recipeRoutes.post("/", createRecipeController.handle);
 
 recipeRoutes.get("/", listRecipecontroller.handle);
+
+recipeRoutes.delete("/:id", deleteRecipeController.handle);
 
 //= =============Recipes-Ingredients========================
 recipeRoutes.get("/ingredient/:id", listRecipesByIngredientController.handle);
