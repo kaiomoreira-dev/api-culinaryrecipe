f46ccbe02e5e14e0fa478ff295f098ad289274d1
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2022-09-25 21:57:31 -0300

    indent interface IRecipeRepository

--- src/modules/recipe/repositories/IRecipeRepository.ts
+++ src/modules/recipe/repositories/IRecipeRepository.ts
@@ -2,21 +2,21 @@ import { ICreateRecipeDTO } from "../dtos/ICreateRecipeDTO";
 import { Recipe } from "../infra/typeorm/entities/Recipe";
 
 export interface IRecipeRepository {
-    create(data: ICreateRecipeDTO): Promise<Recipe>;
+  create(data: ICreateRecipeDTO): Promise<Recipe>;
 
-    list(): Promise<Recipe[]>;
-    listByDifficulty(difficulty: string): Promise<Recipe[]>;
-    listByAuthorId(author_id: string): Promise<Recipe[]>;
+  list(): Promise<Recipe[]>;
+  listByDifficulty(difficulty: string): Promise<Recipe[]>;
+  listByAuthorId(author_id: string): Promise<Recipe[]>;
 
-    // //=====Recipes-Ingredients======//
-    listRecipesByIngredientId(id: string): Promise<Recipe[]>;
+  // //=====Recipes-Ingredients======//
+  listRecipesByIngredientId(id: string): Promise<Recipe[]>;
 
-    findById(id: string): Promise<Recipe>;
-    findByAuthorId(author_id: string): Promise<Recipe>;
-    findByName(name: string): Promise<Recipe>;
+  findById(id: string): Promise<Recipe>;
+  findByAuthorId(author_id: string): Promise<Recipe>;
+  findByName(name: string): Promise<Recipe>;
 
-    updateTimeById(id: string, time: number): Promise<Recipe>;
-    updateAuthorIdById(id: string, author_id: string): Promise<Recipe>;
+  updateTimeById(id: string, time: number): Promise<Recipe>;
+  updateAuthorIdById(id: string, author_id: string): Promise<Recipe>;
 
-    deleteRecipeById(id: string, ingredient_ids: string[]): Promise<void>;
+  deleteRecipeById(id: string, ingredient_ids: string[]): Promise<void>;
 }
