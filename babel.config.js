1a7decad6ca87067cfa05bd2b38ee7a53a19a8e1
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-02-04 18:34:25 -0300

    Update README updt var example ambiente

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,23 @@
 module.exports = {
-    presets: [
-      ["@babel/preset-env", { targets: { node: "current" } }],
-      "@babel/preset-typescript",
-    ],
-    plugins: [
-      [
-        "module-resolver",
-        {
-          alias: {
-            "@modules": "./src/modules",
-            "@shared": "./src/shared",
-            "@erros": "./src/errors",
-            "@config": "./src/config",
-            "@utils": "./src/utils",
-          },
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          "@modules": "./src/modules",
+          "@shared": "./src/shared",
+          "@erros": "./src/errors",
+          "@config": "./src/config",
+          "@utils": "./src/utils",
         },
-      ],
-      "babel-plugin-transform-typescript-metadata",
-      ["@babel/plugin-proposal-decorators", { legacy: true }],
-      ["@babel/plugin-proposal-class-properties", { loose: true }],
+      },
     ],
-  };
-  
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+  ],
+};
--- src/modules/recipe/infra/typeorm/entities/Ingredient.ts
+++ src/modules/recipe/infra/typeorm/entities/Ingredient.ts
@@ -1,11 +1,11 @@
 import {
-    Column,
-    CreateDateColumn,
-    Entity,
-    JoinColumn,
-    ManyToOne,
-    PrimaryColumn,
-    UpdateDateColumn,
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
 } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
 
@@ -13,35 +13,35 @@ import { Produto } from "./Produto";
 
 @Entity("ingredients")
 export class Ingredient {
-    @PrimaryColumn()
-    id?: string;
+  @PrimaryColumn()
+  id?: string;
 
-    // relacionamento de many-to-one apontando para produto qual
-    // ingrediente deve pertencer a produto, mas não queremos
-    // relacionar um produto com uma lista de ingredients que eles
-    // podem pertecer
-    @ManyToOne(() => Produto, (produto) => produto.ingredients)
-    @JoinColumn({ name: "produto_id" })
-    produto?: Produto;
+  // relacionamento de many-to-one apontando para produto qual
+  // ingrediente deve pertencer a produto, mas não queremos
+  // relacionar um produto com uma lista de ingredients que eles
+  // podem pertecer
+  @ManyToOne(() => Produto, (produto) => produto.ingredients)
+  @JoinColumn({ name: "produto_id" })
+  produto?: Produto;
 
-    @Column()
-    produto_id: string;
+  @Column()
+  produto_id: string;
 
-    @Column()
-    weight: number;
+  @Column()
+  weight: number;
 
-    @Column()
-    unity: number;
+  @Column()
+  unity: number;
 
-    @CreateDateColumn()
-    created_at: Date;
+  @CreateDateColumn()
+  created_at: Date;
 
-    @UpdateDateColumn()
-    updated_at: Date;
+  @UpdateDateColumn()
+  updated_at: Date;
 
-    constructor() {
-        if (!this.id) {
-            this.id = uuidv4();
-        }
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
     }
+  }
 }
